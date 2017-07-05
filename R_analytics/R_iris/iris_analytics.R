3439389d19ad4b3c315c0be0c41cb2a8f115a65c
Edima Udo <edimaudo@gmail.com>
2017-07-05 19:46:31 -0400

    iris_analytics

---
+++ R_analytics/R_iris/iris_analytics.R
@@ -0,0 +1,58 @@
+#iris dataset
+setwd()
+
+#load libraries
+library(caret)
+library(lattice)
+library(ggplot2)
+
+# attach the iris dataset to the environment
+data(iris)
+# rename the dataset
+#dataset <- iris
+
+# define the filename
+filename <- "iris.csv"
+# load the CSV file from the local directory
+dataset <- read.csv(filename, header=FALSE)
+# set the column names in the dataset
+colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
+
+#create validation data set
+# create a list of 80% of the rows in the original dataset we can use for training
+validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+# select 20% of the data for validation
+validation <- dataset[-validation_index,]
+# use the remaining 80% of data to training and testing the models
+dataset <- dataset[validation_index,]
+
+#modelling
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+
+
+#base model
+#summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
+
+
+#predictions
+# estimate skill of LDA on the validation dataset
+predictions <- predict(fit.lda, validation)
+confusionMatrix(predictions, validation$Species)
