46145048dc6688d2affa3c4bbbf7e972ad4e85b8
Edima Udo <edimaudo@gmail.com>
2017-09-02 14:16:52 -0400

    conjoint analysis

---
+++ R_analytics/conjoint.R
@@ -0,0 +1,20 @@
+# Connect to our data 
+myConjointData <- read.csv("~/Desktop/Exercise_Files/06_02/conjoint-r.csv")
+myConjointDataProfilesMatrix <- read.csv("~/Desktop/Exercise_Files/06_02/conjoint-r-profiles-matrix.csv")
+myConjointDataLevelNames <- read.csv("~/Desktop/Exercise_Files/06_02/conjoint-r-level-names.csv")
+
+# Step 1.) Install the conjoint package
+# install.packages("conjoint")
+# Step 2.) Load the conjoint package
+library(conjoint)
+# Step 3 if needed.) If R throws an error, refer to the readme file in the Exercise_Files
+
+# Model some of our data
+
+# Model all of our data
+
+
+
+
+
+
