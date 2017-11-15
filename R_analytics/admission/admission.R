b172fc677407e4b35e145f4e4cffca11e7dd4bde
Edima Udo <edimaudo@gmail.com>
2017-11-15 09:38:26 -0500

    other

--- R_analytics/admission/admission.R
+++
@@ -1,8 +0,0 @@
-#libraries
-library(ggplot2)
-library(corrplot)
-library(lattice)
-library(dplyr)
-library(tidyr)
-library(LogicReg)
-library(caret)
---
+++ other/plot_practice.R
@@ -0,0 +1,8 @@
+mydata <- iris
+mydata2 <- USAccDeaths
+
+
+plot(mydata)
+plot(iris, col=iris$Species)
+
+plot(USAccDeaths, xlab="Year", ylab="Accident Deaths in U.S.")
