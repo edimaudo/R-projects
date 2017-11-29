aee3ff50bb51e20faded17d4b184fa55e5bf920b
Edima Udo <edimaudo@gmail.com>
2017-11-28 21:56:33 -0500

    UK Elections

--- R_analytics/uk_elections/uk_elections.R
+++ R_analytics/uk_elections/uk_elections.R
@@ -10,9 +10,9 @@ library(nFactors)
 library(RColorBrewer)
 library(scales)
 library(NbClust)
-#Q1
+library(ggpubr)
 
-#
+#Q1
 scores <- c(98, 74, 65, 78, 17, 65, 82, 72, 68, 
             74, 59, 95, 51, 94, 39, 34, 68, 17, 90, 
             86, 65, 57, 33, 78, 29, 80, 65, 29, 82, 
@@ -23,12 +23,26 @@ std_scores <- sd(scores)
 
 score_amount <- length(scores)
 
+#95% confidence interval
 error <- qnorm(0.975)*std_scores/sqrt(score_amount)
-left <- mean_scores - error
-right <- mean_scores + error
+left95 <- mean_scores - error
+right95 <- mean_scores + error
+n95 <- c(left95,right95)
 
-#test for 95% and 99% confidence interval
+#99% confidence interval
+error99 <- qnorm(0.995)*std_scores/sqrt(score_amount)
+left99 <- mean_scores - error99
+right99 <- mean_scores + error99
+n99 <- c(left99,right99)
 
-#Q2 compare the difference in means
-#what is the hypothesis ->
-# might wantnt o use t -test
+#Q2 
+#use unpaired two sample t-test
+#The unpaired two-samples t-test is used to compare the mean of two independent groups.
+
+#hypothesis test
+#whether the mean of group A (mAmA) is greather than the mean of group B (mBmB)?
+#can't compute do welch t-test manually
+
+#Q4
+#look at the p-values of the different coefficients to explain the impact
+#Also look at the r-square value to see how much it interrpets
