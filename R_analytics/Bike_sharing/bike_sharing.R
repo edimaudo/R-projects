2bd88e82c83799e23e9f3e3aa643ec5b098c01fb
Edima Udo <edimaudo@gmail.com>
2017-11-30 18:54:54 -0500

    bike sharing

--- R_analytics/Bike_sharing/bike_sharing.R
+++ R_analytics/Bike_sharing/bike_sharing.R
@@ -55,15 +55,18 @@ ggplot(daily_data, aes(Date, cnt)) + geom_line() + scale_x_date('month')
   xlab("")
 
 # moving average
-daily_data$cnt_ma = ma(daily_data$clean_cnt, order=7) # using the clean count with no outliers
+daily_data$cnt_ma = ma(daily_data$clean_cnt, order=7) 
+# using the clean count with no outliers
 daily_data$cnt_ma30 = ma(daily_data$clean_cnt, order=30)
 
 
 ggplot() +
   geom_line(data = daily_data, aes(x = Date, y = clean_cnt, colour = "Counts")) +
-  geom_line(data = daily_data, aes(x = Date, y = cnt_ma,   colour = "Weekly Moving Average")) 
+  geom_line(data = daily_data, aes(x = Date, y = cnt_ma,   
+                                   colour = "Weekly Moving Average")) 
 +
-  geom_line(data = daily_data, aes(x = Date, y = cnt_ma30, colour = "Monthly Moving Average"))  
+  geom_line(data = daily_data, aes(x = Date, y = cnt_ma30, 
+                                   colour = "Monthly Moving Average"))  
 +
   ylab('Bicycle Count')
 
