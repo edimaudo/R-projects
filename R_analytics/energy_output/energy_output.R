5f9cf9cde7feebea30e0951778ac143f53519ed5
Edima Udo <edimaudo@gmail.com>
2017-12-05 11:31:06 -0500

    energy_output

---
+++ R_analytics/energy_output/energy_output.R
@@ -0,0 +1,15 @@
+library("readxl")
+
+powerData <- read_excel(file.choose())
+
+head(powerData)
+
+set.seed(123)
+split <- sample(nrow(powerData), size = floor(0.75 * nrow(powerData)))
+trainData <- powerData[split, ]
+testData <- powerData[-split, ]
+head(trainData)
+head(testData)
+
+predictionModel <- lm(PE ~ ., data = trainData)
+summary(predictionModel)
