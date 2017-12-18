d0f2f8b4fcc36c241155789793ca619736a10b47
Edima Udo <edimaudo@gmail.com>
2017-12-18 10:33:39 -0500

    Happiness
    simple and multivariate linear regression models

---
+++ R_analytics/happiness/happiness.R
@@ -0,0 +1,31 @@
+#load libraries
+library(cluster)
+library(ggplot2)
+library(factoextra)
+library(corrplot)
+library(psy)
+library(lattice)
+library(nFactors)
+library(RColorBrewer)
+library(scales)
+
+
+#remove all data
+rm(list=ls())
+
+#use filechooser to select csv file
+mydata <- read.csv(file.choose(), sep = ',')
+
+#simple linear regression
+output <- lm(LIFE ~ CORRUPT, data = mydata)
+summary(output)
+resid(output) #List of residuals
+plot(density(resid(output))) #A density plot
+qqnorm(resid(output))
+
+output2 <- lm(log(LIFE) ~ CORRUPT, data = mydata)
+summary(output2)
+resid(output2) #List of residuals
+plot(density(resid(output2))) #A density plot
+qqnorm(resid(output2))
+
