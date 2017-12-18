6ffcd9fbcebc407cc82df410f1f51260ddec7e20
Edima Udo <edimaudo@gmail.com>
2017-12-18 10:45:37 -0500

    happiness

--- R_analytics/happiness/happiness.R
+++ R_analytics/happiness/happiness.R
@@ -17,15 +17,16 @@ rm(list=ls())
 mydata <- read.csv(file.choose(), sep = ',')
 
 #simple linear regression
-output <- lm(LIFE ~ CORRUPT, data = mydata)
+output <- lm(LSI ~ CORRUPT, data = mydata)
 summary(output)
 resid(output) #List of residuals
 plot(density(resid(output))) #A density plot
 qqnorm(resid(output))
 
-output2 <- lm(log(LIFE) ~ CORRUPT, data = mydata)
+output2 <- lm(log(LSI) ~ CORRUPT, data = mydata)
 summary(output2)
 resid(output2) #List of residuals
 plot(density(resid(output2))) #A density plot
 qqnorm(resid(output2))
+
 
