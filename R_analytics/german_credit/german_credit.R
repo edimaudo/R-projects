942f1ba591a27fe1d61e6d5d5f33ee6766ed05b4
Edima Udo <edimaudo@gmail.com>
2017-12-18 10:33:10 -0500

    German credit
    simple logistic regression model

--- R_analytics/german_credit/german_credit.R
+++ R_analytics/german_credit/german_credit.R
@@ -7,7 +7,7 @@ library(tidyr)
 library(LogicReg)
 library(caret)
 library(readxl)
-
+library(pROC)
 #==========================
 #logistic regression
 #==========================
@@ -27,3 +27,24 @@ mydata <- na.omit(mydata) # listwise deletion of missing
 
 #set seed
 set.seed(1)
+
+splitIndex <- createDataPartition(mydata$Default, p = .80,list = FALSE, times = 1)
+trainSplit <- mydata[ splitIndex,]
+testSplit <- mydata[-splitIndex,]
+print(table(trainSplit$Default))
+
+
+#logistic regression
+ctrl <- trainControl(method = "cv", number = 5)
+modelglm <- train(Default ~. , data = trainSplit, method = "glm", trControl = ctrl)
+summary(modelglm)
+### predict
+predictors <- names(trainSplit)[names(trainSplit) != 'Default']
+predglm <- predict(modelglm, testSplit)
+summary(predglm)
+### score prediction using AUC
+confusionMatrix(predglm, testSplit$Default)
+
+aucglm <- roc(as.numeric(testSplit$Default), as.numeric(predglm),  ci=TRUE)
+plot(aucglm, ylim=c(0,1), print.thres=TRUE, 
+     main=paste('Logistic Regression AUC:',round(aucglm$auc[[1]],3)),col = 'blue')
