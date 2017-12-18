36e523440f0048ea1aad95dcc9c6f398a0a39efb
Edima Udo <edimaudo@gmail.com>
2017-12-18 10:50:00 -0500

    Gesell

--- R_analytics/gesell/gesell.R
+++ R_analytics/gesell/gesell.R
@@ -4,6 +4,9 @@ rm(list=ls())
 #use filechooser to select csv file
 mydata <- read.csv(file.choose(), sep = ',')
 
+library(ggplot2)
+ggplot(mydata, aes(x = Age, y = Gesell.score)) +geom_boxplot()
+
 #simple linear regression
 output <- lm(Gesell.score ~ Age, data = mydata)
 summary(output)
