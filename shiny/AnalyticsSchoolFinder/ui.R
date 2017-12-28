312bcb9757fdd39be996f278a48626383c78909f
Edima Udo <edimaudo@gmail.com>
2017-12-28 14:39:11 -0500

    Analytics School Finder

--- shiny/AnalyticsSchoolFinder/ui.R
+++ shiny/AnalyticsSchoolFinder/ui.R
@@ -1,19 +1,19 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 
 shinyUI(
   pageWithSidebar(
-    headerPanel()
-  ), 
-  mainPanel(
-    
+    headerPanel('Analytics/Data Science Programs in Canada'),
+    sidebarPanel(
+      sliderInput('endHour', 'Choose the ending hour', min = 0, max = 23, value = 7)
+    ),
+    mainPanel(
+      h4('Options'),
+      tabsetPanel(
+        tabPanel("Weight", plotOutput("plot")),
+        tabPanel("Program Information", tableOutput("table"))
+      )
+    )
   )
 )
