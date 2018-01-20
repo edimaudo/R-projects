bcfc88216e9507fc075a5553268f11f626e8a2e5
Edima Udo <edimaudo@gmail.com>
2018-01-19 21:25:52 -0500

    Market Basket Analysis

---
+++ R_analytics/Market Basket Analysis/Market Basket Analysis.R
@@ -0,0 +1,27 @@
+#scene data analysis
+
+#verticals
+#overall
+#high value
+#scentourage members
+
+#libraries
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(data.table)
+#data
+
+#exploratory analysis
+
+#rfm analysis
+
+#rfm visualization
+
+#market basket analysis
+
+#rules
+
+#recommendations
+
+#visaulize recommendations
