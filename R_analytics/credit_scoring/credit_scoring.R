c972e8bf61545c3d078913ee174d93cc20fa8cb0
Edima Udo <edimaudo@gmail.com>
2018-01-29 07:45:39 -0500

    credit scoring

---
+++ R_analytics/credit_scoring/credit_scoring.R
@@ -0,0 +1,22 @@
+# remove old data
+
+#load libraries
+library(tidyverse)
+library(ggplot2)
+
+#load data
+
+#exploratory analysis
+
+#modeling
+#logistic regression
+
+#decision trees
+
+#random forest
+
+#ANN
+
+#support vector machines
+
+#
