53893ddc4fb4905c5e7a8ee688486bb48b4b7a64
Edima Udo <edimaudo@gmail.com>
2018-01-30 14:20:36 -0500

    credit scoring

--- R_analytics/credit_scoring/credit_scoring.R
+++ R_analytics/credit_scoring/credit_scoring.R
@@ -1,15 +1,43 @@
 # remove old data
+rm(list=ls())
 
 #load libraries
 library(tidyverse)
 library(ggplot2)
+library(pROC)
+library(LogicReg)
+library(randomForest)
+library(neuralnet)
+library(GGally)
+library(corrplot)
 
 #load data
+training <- read.csv(file.choose(), sep = ",")
+test <- read.csv(file.choose(), sep = ",")
 
-#exploratory analysis
+#explore
+glimpse(training)
+glimpse(test)
+
+names(training)
+
+#remove the first columns
+training[1] <- NULL
+test[1] <- NULL
+
+#backup 
+training.orig <- training
+test.orig <- test
+
+
+#remove na since data imputation would give odd results
+training <- na.omit(training)
+
+
 
 #modeling
 #logistic regression
+
 
 #decision trees
 
@@ -19,4 +47,7 @@ library(ggplot2)
 
 #support vector machines
 
-#
+
+#compare models
+
+#model AUC
