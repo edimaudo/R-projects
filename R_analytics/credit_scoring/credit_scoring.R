0f8327e0a05143ea2c2158d3d00eb81ada549956
Edima Udo <edimaudo@gmail.com>
2018-02-01 19:08:18 -0500

    credit scoring

--- R_analytics/credit_scoring/credit_scoring.R
+++ R_analytics/credit_scoring/credit_scoring.R
@@ -10,44 +10,61 @@ library(randomForest)
 library(neuralnet)
 library(GGally)
 library(corrplot)
+library(caret)
+library(e1071)
 
 #load data
-training <- read.csv(file.choose(), sep = ",")
+train <- read.csv(file.choose(), sep = ",")
 test <- read.csv(file.choose(), sep = ",")
 
 #explore
-glimpse(training)
+glimpse(train)
 glimpse(test)
 
 names(training)
 
 #remove the first columns
-training[1] <- NULL
+train[1] <- NULL
 test[1] <- NULL
 
 #backup 
-training.orig <- training
+train.orig <- train
 test.orig <- test
 
-
 #remove na since data imputation would give odd results
-training <- na.omit(training)
+train <- na.omit(train)
 
+#correlation
+#corinfo <- training[,2:11]
+#corrplot(cor(corinfo), method="number")
 
+set.seed(123)
+
+glimpse(train)
+glimpse(test)
 
 #modeling
-#logistic regression
 
+#logistic regression
+ctrl <- trainControl(method = "cv", number = 5)
+modelglm <- train(as.factor(SeriousDlqin2yrs) ~. , data = train, method = "glm", trControl = ctrl)
+summary(modelglm)
+### predict
+predictors <- names(train)[names(train) != 'SeriousDlqin2yrs']
+predglm <- predict(modelglm, train)
+summary(predglm)
+### score prediction using AUC
+confusionMatrix(predglm, train$SeriousDlqin2yrs)
+
+aucglm <- roc(as.numeric(train$SeriousDlqin2yrs), as.numeric(predglm),  ci=TRUE)
+plot(aucglm, ylim=c(0,1), print.thres=TRUE, 
+     main=paste('Logistic Regression AUC:',round(aucglm$auc[[1]],3)),col = 'blue')
 
 #decision trees
-
-#random forest
-
-#ANN
-
-#support vector machines
+ctrl <- trainControl(method = "cv", number = 5)
+tbmodel <- train(as.factor(SeriousDlqin2yrs) ~., data = train, method = "C5.0Tree", trControl = ctrl)
+pred <- predict(tbmodel, train)
+confusionMatrix(pred,train$SeriousDlqin2yrs)
 
 
-#compare models
 
-#model AUC
