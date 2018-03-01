e05eb202d7d72e2a52b7f6c3d1f5329e780849e9
Edima Udo <edimaudo@gmail.com>
2018-02-28 20:31:03 -0500

    health care reform

--- R_analytics/health_care/hsr.R
+++ R_analytics/health_care/hsr.R
@@ -1,4 +1,4 @@
-# 2.4K tweets on US health care reform
+# 2.4K wordsInfo on US health care reform
 # Labeled by humans as either positive or negative relative to a certain entity:
 #   hcr, liberals, other, stupak, dems, teaparty, conservatives, gop, obama
 # Possible task: Predict whether a tweet supports health care reform?
@@ -37,56 +37,94 @@ ggplot(hcr, aes(x=reorder(as.factor(sentiment), -table(sentiment)[sentiment]))) 
   geom_bar(fill="steelblue")+
   theme_minimal()
 
-#=====================
-#prediction
-#=====================
-#recode data
-
-#check for correlation
-
-#=============
-#without text
-#============
-#create train and test data
-
-#logistic regression
-#decision trees
-#LDA
-#svm
-#random forest
-#neural network
-#boosting algorithm
-
-#===================
-#compare models
-#==================
-
-
-
-
-
 #------------------
-#with text
+#sentiment analysis
 #------------------
-#clean text function
-#logistic regression
-#decision trees
-#LDA
-#random forest
-#neural network
-#boosting algorithm
+library(RTextTools)
+library(e1071)
+library(tm)
+library(SnowballC)
 
-#compare models
+text.clean = function(x)                    # text data
+{ 
+  x  =  gsub("<.*?>", " ", x)               # regex for removing HTML tags
+  x  =  iconv(x, "latin1", "ASCII", sub="") # Keep only ASCII characters
+  x  =  gsub("[^[:alnum:]]", " ", x)        # keep only alpha numeric 
+  x  =  tolower(x)                          # convert to lower case characters
+  x  =  removeNumbers(x)                    # removing numbers
+  x  =  stripWhitespace(x)                  # removing white space
+  x  =  gsub("^\\s+|\\s+$", "", x)          # remove leading and trailing white space
+  return(x)
+}
+
+#clean text
+hcr$tweet_info <- text.clean(hcr$tweet_info)
+
+library(tidyverse)
+
+#sentiment analysis
+positive_words <- hcr %>%
+  filter(sentiment == "positive") %>%
+  select(tweet_info,sentiment)
+
+negative_words <- hcr %>%
+  filter(sentiment == "negative") %>%
+  select(tweet_info,sentiment)
+
+test <- hcr %>%
+  sample_frac(size = 0.2, replace = FALSE, weight = NULL, .env = NULL) %>%
+  select(tweet_info,sentiment)
+
+wordsInfo = rbind(positive_words, negative_words, test)
+
+#build document term matrix
+# build dtm
+matrix= create_matrix(wordsInfo[,1], language="english", 
+                      removeStopwords=FALSE, removeNumbers=TRUE, 
+                      stemWords=FALSE)
+
+#naive bayes model
+# train the model
+mat = as.matrix(matrix)
+classifier = naiveBayes(mat[1:10,], as.factor(wordsInfo[1:10,2]) )
+# test the validity
+predicted = predict(classifier, mat[11:15,]); predicted
+table(wordsInfo[11:15, 2], predicted)
+recall_accuracy(wordsInfo[11:15, 2], predicted)
+
+# build the data to specify response variable, training set, testing set.
+container = create_container(matrix, as.numeric(as.factor(wordsInfo[,2])),
+                             trainSize=1:10, testSize=11:15,virgin=FALSE)
 
 
-#------------------
-#prediction using sentiment analysis
-#------------------
+#Second, to train the model with multiple machine learning algorithms:
+models = train_models(container, algorithms=c("MAXENT" , "SVM", "RF", "BAGGING", "TREE"))
 
-#------------------
-#word cloud
-#------------------
+results = classify_models(container, models)
 
-#------------------
-#word clustering
-#------------------
+  
+# accuracy table
+table(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"FORESTS_LABEL"])
+table(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"MAXENTROPY_LABEL"])
+
+# recall accuracy
+recall_accuracy(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"FORESTS_LABEL"])
+recall_accuracy(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"MAXENTROPY_LABEL"])
+recall_accuracy(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"TREE_LABEL"])
+recall_accuracy(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"BAGGING_LABEL"])
+recall_accuracy(as.numeric(as.factor(wordsInfo[11:15, 2])), results[,"SVM_LABEL"])
+
+  
+# model summary
+analytics = create_analytics(container, results)
+summary(analytics)
+head(analytics@document_summary)
+#analytics@ensemble_summar
+
+#To cross validate the results:
+N=4
+set.seed(2014)
+cross_validate(container,N,"MAXENT")
+cross_validate(container,N,"TREE")
+cross_validate(container,N,"SVM")
+cross_validate(container,N,"RF")
