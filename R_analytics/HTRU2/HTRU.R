6cd441ae1cebd53a3c567cb5ebb4f025a21cc8f7
Edima Udo <edimaudo@gmail.com>
2018-03-29 06:00:40 -0400

    HTRU
    created feature selection

--- R_analytics/HTRU2/HTRU.R
+++ R_analytics/HTRU2/HTRU.R
@@ -71,7 +71,7 @@ mydata$class <- as.factor(mydata$class)
 #find important features
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
 # train the model
-model <- train(class~., data=mydata, method="glm", preProcess="scale", trControl=control)
+model <- train(class~., data=mydata, method="lvq", preProcess="scale", trControl=control)
 # estimate variable importance
 importance <- varImp(model, scale=FALSE)
 # summarize importance
@@ -79,6 +79,26 @@ print(importance)
 # plot importance
 plot(importance)
 
+
+#RFE for feature selection
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the data
+data(PimaIndiansDiabetes)
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(mydata[,1:8], mydata[,9], sizes=c(1:8), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
 #modelling approach
+
 
 #fine tune model
