3f1c7d3b46b0964adf930d11316fb6e351293e86
Edima Udo <edimaudo@gmail.com>
2018-03-29 14:01:37 -0400

    air_quality

--- R_analytics/air_quality/air_quality.R
+++ R_analytics/air_quality/air_quality.R
@@ -6,3 +6,27 @@ library(readxl)
 library(tidyverse)
 library(caret)
 library(data.table)
+
+#read data
+airQ = as.data.frame(read_excel(file.choose())) #excel file
+
+#backup
+airQ.orig <- airQ
+
+#sort by wind
+airQ <- airQ %>%
+  arrange(Wind)
+
+#remove where ozone is NA
+airQ <- airQ %>%
+  filter(Ozone != "NA")
+
+#Remove the Solar.R column and create a new column containing the ratio of Ozone to Wind.
+airQ <- airQ %>%
+  select (-Solar.R) %>%
+  mutate(Ozone = as.numeric(Ozone)) %>%
+  mutate(ozone_wind_ratio = Ozone / Wind) 
+  
+library(ggplot2)
+
+  
