b785433b70b9a1adac4ebe7ced5d6586073a8b0a
Edima Udo <edimaudo@gmail.com>
2018-04-13 14:53:19 -0400

    Oxford
    added some possible tasks

--- R_analytics/oxford/Oxford.R
+++ R_analytics/oxford/Oxford.R
@@ -1,5 +1,19 @@
 # Oxford 
-
+# Oxford 
+# Dataset contains two primary tables:
+# Visits
+# Date
+# User meta data: postal code, gender, age-range
+# Unordered list of stores visited
+# Name of store, length of time stayed
+# Store
+# Metadata about each store
+# Distance to each other store; distance to entrance; etc.
+# Your tasks: analyze the data to find interesting patterns that Oxford Properties might be interested in
+# Which stores are visited together most/least frequently?
+# Do they change by time of day, day of week, time of year?
+# Which stores are most frequently visited together that aren't that close together?
+#analysis - association rules + recommendation
 #remove old data
 rm(list=ls())
 
