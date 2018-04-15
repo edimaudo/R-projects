82c093778e76af94cfa8de1aca7ca4a37bef402f
Edima Udo <edimaudo@gmail.com>
2018-04-15 18:01:03 -0400

    txn_rfm
    rfm data created

--- R_analytics/rfm/txn_rfm.R
+++ R_analytics/rfm/txn_rfm.R
@@ -5,12 +5,29 @@ rm(list=ls())
 library(tidyverse)
 library(ggplot2)
 library(readxl)
+library(lubridate)
 
 #load excel file
 df <- as.data.frame(read_excel(file.choose()))
 
 #view data
+glimpse(df)
 
 #find missing
+apply(df, 2, function(x) any(is.na(x)))
 
 #do rfm model
+rfm_data <- df %>%
+  select(CUSTNO,TRANDATE,SALESTXN) %>%
+  drop_na()
+
+#convert date
+rfm_data <- rfm_data %>%
+  mutate(TRANDATE = as.Date(TRANDATE,"%Y-%m-%d"))
+
+
+rfm_data2 <- rfm_data %>%
+  group_by(CUSTNO) %>%
+  summarise(recency=(as.numeric(as.Date(today())-max(TRANDATE))),
+            frequency=n(), monetary= sum(SALESTXN))
+
