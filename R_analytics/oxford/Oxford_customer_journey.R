a1c05334639c933ae7f1ee524624b0c23d1521b9
Edima Udo <edimaudo@gmail.com>
2018-04-16 17:36:11 -0400

    oxford customer journey
    added possible analytics ideas

--- R_analytics/oxford/Oxford_customer_journey.R
+++ R_analytics/oxford/Oxford_customer_journey.R
@@ -13,3 +13,7 @@ library(ggplot2)
 #visualization
 
 #type of analysis
+#combine sales, distance and space information
+#can create a predictive model or time series model
+#model will predictive sales or foot traffic
+#can also use association rules for stoes
