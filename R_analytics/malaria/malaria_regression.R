f4f4b1537b10fd2c3a2bbfac7d9729ef5c60ce19
Edima Udo <edimaudo@gmail.com>
2018-04-23 15:50:12 -0400

    malaria regression
    checked for missing data, split data and checked correlation

--- R_analytics/malaria/malaria_regression.R
+++ R_analytics/malaria/malaria_regression.R
@@ -11,3 +11,30 @@ library(data.table)
 #read data
 df = as.data.frame(read_excel(file.choose())) #excel file
 
+#view data
+glimpse(df)
+
+#check for missing data
+apply(df, 2, function(x) any(is.na(x))) #no missing data
+
+#split data into training and test
+set.seed(123)
+train<-sample_frac(df, 0.8)
+sid<-as.numeric(rownames(train)) # because rownames() returns character
+test<-df[-sid,]
+
+#check for correlations
+library(corrplot)
+corrplot(cor(train), method = "number")
+
+#build initial model
+
+
+#check for importance variables
+
+#build new model
+
+#check accuracy and AIC
+
+#test using test data
+
