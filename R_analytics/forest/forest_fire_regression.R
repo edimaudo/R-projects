ee63b2e468bd706a342422f7813aa427906384d5
Edima Udo <edimaudo@gmail.com>
2018-04-26 17:36:07 -0400

    forest fire regression
    read data and split data into train and test

--- R_analytics/forest/forest_fire_regression.R
+++ R_analytics/forest/forest_fire_regression.R
@@ -8,3 +8,15 @@ library(caret)
 library(mlbench)
 library(data.table)
 
+#load data
+df = read.csv(file.choose())
+
+#view data
+glimpse(df)
+
+#check for missing data
+apply(df, 2, function(x) any(is.na(x))) #no missing data
+
+#set seed for reproducability
+set.seed(123)
+
