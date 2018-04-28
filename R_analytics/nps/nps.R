d8b2fca3dff9afacc6f56ead71e33f9bdbb825ea
Edima Udo <edimaudo@gmail.com>
2018-04-28 04:22:54 -0400

    nps

--- R_analytics/nps/nps.R
+++ R_analytics/nps/nps.R
@@ -11,6 +11,7 @@ library(mlbench)
 library(data.table)
 library(corrplot)
 library(pastecs)
+library(car)
 
 #load nps data
 df <- read.csv(file.choose(), sep=";")
@@ -34,6 +35,7 @@ chisq.test(table(df$Region, df$REC))
 
 
 # multiple linear regression - dependdent variable is REC
+# note - use either BU or PRODUCT but not both
 
 levels(df$BU)
 levels(df$PRODUCT)
@@ -45,12 +47,75 @@ df <- df %>%
   select(REC,Salesman.Friendliness,Salesman.Product.Knowledge, Salesman.Professionality,
          Product.Tastiness, PRODUCT, Account, Region)
 
+#split data
+train<-sample_frac(df, 0.8)
+sid<-as.numeric(rownames(train)) # because rownames() returns character
+test<-df[-sid,]
+
+#
+
 #model 1
-# outliers, non normality
-# note - use either BU or PRODUCT but not both
-# note -  check linear regression assumptions
-# - split data into test and train
-# - initial model using train data
-# - find important variables
-# - build new model and test
-# - get AIC, accuracy
+fit <- lm(REC ~., data=train)
+summary(fit)
+AIC(fit)
+
+#regression assumptions
+# Assessing Outliers
+outlierTest(fit) # Bonferonni p-value for most extreme obs
+qqPlot(fit, main="QQ Plot") #qq plot for studentized resid 
+leveragePlots(fit) # leverage plots
+
+# Influential Observations
+# added variable plots 
+av.Plots(fit)
+# Cook's D plot
+# identify D values > 4/(n-k-1) 
+cutoff <- 4/((nrow(mtcars)-length(fit$coefficients)-2)) 
+plot(fit, which=4, cook.levels=cutoff)
+# Influence Plot 
+influencePlot(fit,	id.method="identify", main="Influence Plot", 
+              sub="Circle size is proportial to Cook's Distance" )
+
+# Normality of Residuals
+# qq plot for studentized resid
+qqPlot(fit, main="QQ Plot")
+# distribution of studentized residuals
+library(MASS)
+sresid <- studres(fit) 
+hist(sresid, freq=FALSE, 
+     main="Distribution of Studentized Residuals")
+xfit<-seq(min(sresid),max(sresid),length=40) 
+yfit<-dnorm(xfit) 
+lines(xfit, yfit)
+
+# Evaluate homoscedasticity
+# non-constant error variance test
+ncvTest(fit)
+# plot studentized residuals vs. fitted values 
+spreadLevelPlot(fit)
+
+# Evaluate Collinearity
+vif(fit) # variance inflation factors 
+sqrt(vif(fit)) > 2 # problem?
+
+# Evaluate Nonlinearity
+# component + residual plot 
+crPlots(fit)
+# Ceres plots 
+ceresPlots(fit)
+
+# Test for Autocorrelated Errors
+durbinWatsonTest(fit)
+
+#test for features
+# Calculate Relative Importance for Each Predictor
+library(relaimpo)
+calc.relimp(fit,type=c("lmg","last","first","pratt"),
+            rela=TRUE)
+
+# Bootstrap Measures of Relative Importance (1000 samples) 
+boot <- boot.relimp(fit, b = 1000, type = c("lmg", 
+                                            "last", "first", "pratt"), rank = TRUE, 
+                    diff = TRUE, rela = TRUE)
+booteval.relimp(boot) # print result
+plot(booteval.relimp(boot,sort=TRUE)) # plot result
