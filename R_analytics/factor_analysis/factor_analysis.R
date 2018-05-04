52b702f6ce3d234a35044762b6c0de46ae9e0fd8
Edima Udo <edimaudo@gmail.com>
2018-05-04 00:06:18 -0400

    factor analysis load data

--- R_analytics/factor_analysis/factor_analysis.R
+++ R_analytics/factor_analysis/factor_analysis.R
@@ -1,12 +1,10 @@
-#For the dataset provided, do the following:
+#Determine the minimum number of factors needed given a 90% confidence level
 
 #Perform factor analysis for all variables to determine the top 4 most unique variables 
 #using 12 factors
 
-#Determine the minimum number of factors needed given a 90% confidence level
 
-#Provide the scores of all subjects in the top 10 factors 
-#(performing factor analysis using 15 factors)
+
 
 #-need to use the "factanal" library
 
@@ -25,3 +23,6 @@ library(factoextra)
 library(corrplot)
 library(lattice)
 library(NbClust)
+
+#load data
+mydata <- read.csv(file.choose(), sep = ',') #load csv file
