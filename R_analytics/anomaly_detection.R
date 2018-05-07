b19fdacdfab77794d617735b6536d59c1856c9e5
Edima Udo <edimaudo@gmail.com>
2018-05-07 19:36:28 -0400

    added time series decomposition and anomaly detection portion

--- R_analytics/anomaly_detection.R
+++ R_analytics/anomaly_detection.R
@@ -5,3 +5,22 @@ library(anomalize) #tidy anomaly detectiom
 library(tidyverse) #tidyverse packages like dplyr, ggplot, tidyr
 library(coindeskr) #bitcoin price extraction from coindesk
 
+#extract data
+btc <- get_historic_price(start = "2018-01-01")
+
+#data preprocessing
+btc_ts <- btc %>% rownames_to_column() %>% as.tibble() %>% 
+  mutate(date = as.Date(rowname)) %>% select(-one_of('rowname'))
+
+#time series decomposition
+btc_ts %>% 
+  time_decompose(Price, method = "stl", frequency = "auto", trend = "auto") %>%
+  anomalize(remainder, method = "gesd", alpha = 0.05, max_anoms = 0.2) %>%
+  plot_anomaly_decomposition()
+
+#anomaly detection
+btc_ts %>% 
+  time_decompose(Price) %>%
+  anomalize(remainder) %>%
+  time_recompose() %>%
+  plot_anomalies(time_recomposed = TRUE, ncol = 3, alpha_dots = 0.5)
