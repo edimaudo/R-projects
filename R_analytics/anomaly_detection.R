8e31566ed6502bcd19448c288eab1e8d31bf9cd6
Edima Udo <edimaudo@gmail.com>
2018-05-07 19:34:34 -0400

    added libraries

--- R_analytics/anomaly_detection.R
+++ R_analytics/anomaly_detection.R
@@ -1,3 +1,7 @@
 
 #Bitcoin Price Anomaly Detection
 
+library(anomalize) #tidy anomaly detectiom
+library(tidyverse) #tidyverse packages like dplyr, ggplot, tidyr
+library(coindeskr) #bitcoin price extraction from coindesk
+
