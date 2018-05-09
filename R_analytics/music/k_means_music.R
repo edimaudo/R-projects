16e0c4f708c153020e76d22a404e338c84f5f30e
Edima Udo <edimaudo@gmail.com>
2018-05-09 13:47:31 -0400

    load libraries

---
+++ R_analytics/music/k_means_music.R
@@ -0,0 +1,16 @@
+#clear old data
+rm(list=ls())
+
+#load libraries
+library(psy)
+library(nFactors)
+library(MASS)
+library(psych)
+library(GPArotation)
+library(cluster)
+library(ggplot2)
+library(factoextra)
+library(corrplot)
+library(lattice)
+library(NbClust)
+
