2c767d2138235092bd619b50733776087401bcb2
Edima Udo <edimaudo@gmail.com>
2018-05-09 14:04:06 -0400

    added code to find optimal number of clusters

--- R_analytics/music/k_means_music.R
+++ R_analytics/music/k_means_music.R
@@ -33,6 +33,25 @@ mydata[1] <- NULL #remove the first column
 set.seed(1)
 
 #find optimal number of clusters
+#using wss
+fviz_nbclust(mydata, kmeans, method = "wss")
+
+#using average silhouette
+k.max <- 15
+sil <- rep(0, k.max)
+# Compute the average silhouette width for 
+# k = 2 to k = 15
+for(i in 2:k.max){
+  km.res <- kmeans(load, centers = i, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(load))
+  sil[i] <- mean(ss[, 3])
+}
+# Plot the  average silhouette width
+plot(1:k.max, sil, type = "b", pch = 19, 
+     frame = FALSE, xlab = "Number of clusters k")
+abline(v = which.max(sil), lty = 2)
+
+
 
 #number of clusters
 nclusters = 3
