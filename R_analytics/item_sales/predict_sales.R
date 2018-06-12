74c7dea5ceb74e0c27ac0a9b09b6e7fea5a0ba21
Edima Udo <edimaudo@gmail.com>
2018-06-11 21:26:53 -0400

    more data cleaning

--- R_analytics/item_sales/predict_sales.R
+++ R_analytics/item_sales/predict_sales.R
@@ -37,9 +37,29 @@ missing_data_item_cat <- apply(item_categories, 2, function(x) any(is.na(x)))
 missing_data_items <- apply(items, 2, function(x) any(is.na(x)))
 missing_data_shops <- apply(shops, 2, function(x) any(is.na(x)))
 
+#data cleaning
+consolidated = sales_train %>%
+  left_join(items, by=c("item_id"))
+
+###Since we are not using name,will remove item name
+consolidated$item_name=NULL
+
+###is there any Missing obesrvation
+colSums(is.na(consolidated))
+
+consolidated$date=dmy(consolidated$date)
+consolidated$month=month(consolidated$date)
+consolidated$year=year(consolidated$date)
+consolidated$day=weekdays(consolidated$date)
+
+consolidated$day=as.factor(consolidated$day)
+consolidated$month=as.factor(consolidated$month)
+consolidated$year=as.factor(consolidated$year)
+
+glimpse(consolidated)
+
 #data exploration
 
-#data cleaning
 
 #perform forecasting
 
