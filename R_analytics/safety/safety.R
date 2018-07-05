b3f0436d49400e474fd761cace5fba086e24c265
Edima Udo <edimaudo@gmail.com>
2018-07-05 15:32:29 -0400

    added libraries

--- R_analytics/safety/safety.R
+++ R_analytics/safety/safety.R
@@ -1,2 +1,16 @@
 # remove old data
 rm(list=ls())
+
+#libraries
+library(corrplot)
+library(tidyverse)
+library(lubridate)
+library(wordcloud2)
+library(data.table)
+library(stringr)
+library(mlbench)
+library(caret)
+library(xgboost)
+library(tidytext)
+
+
