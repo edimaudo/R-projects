d245bd0188ede69c7a8a52273f4d37cdce72c0c2
Edima Udo <edimaudo@gmail.com>
2018-07-05 15:39:47 -0400

    update
    added an objective, checked for missing data, added section for
    visualization, added section for building the classifier

--- R_analytics/safety/safety.R
+++ R_analytics/safety/safety.R
@@ -1,3 +1,6 @@
+#objective
+#build a text classifier in ( R ) for safety observations to classify the (object) and (failure mode).
+
 # remove old data
 rm(list=ls())
 
@@ -15,3 +18,24 @@ library(tidytext)
 
 #load safety data
 df <- read.csv(file.choose(), stringsAsFactors = FALSE)
+
+#check for missing data
+missing_data <- apply(df, 2, function(x) any(is.na(x))) #no missing data
+
+#==============
+#visualization
+#==============
+
+#visualize object
+
+#visualize failure
+
+#visualize object and failure
+
+#clean text
+
+#visualize text using word cloud
+
+#==============
+#build classifier
+#==============
