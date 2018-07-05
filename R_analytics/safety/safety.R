de59039edb3f16e959a0f3e938e42b3d39452ed7
Edima Udo <edimaudo@gmail.com>
2018-07-05 19:01:50 -0400

    build barplots for object and failure

--- R_analytics/safety/safety.R
+++ R_analytics/safety/safety.R
@@ -27,10 +27,26 @@ missing_data <- apply(df, 2, function(x) any(is.na(x))) #no missing data
 #==============
 
 #visualize object
+df %>%
+  count(Object, sort = TRUE) %>%
+  filter(n > 50) %>%
+  ggplot(aes(Object, n)) +
+  geom_col() +
+  ylab("Count") +
+  xlab("Object information") + theme_classic()
 
 #visualize failure
+df %>%
+  count(Failure, sort = TRUE) %>%
+  filter(n > 50) %>%
+  ggplot(aes(Failure, n)) +
+  geom_col() +
+  ylab("Count") +
+  xlab("Failure information") + theme_classic()
 
-#visualize object and failure
+#==============
+#world cloud
+#==============
 
 #clean text
 
