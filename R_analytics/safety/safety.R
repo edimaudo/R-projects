724b5426f07d8e01575c42ff86039619ba83d212
Edima Udo <edimaudo@gmail.com>
2018-07-05 22:40:28 -0400

    Added code for topic clustering

--- R_analytics/safety/safety.R
+++ R_analytics/safety/safety.R
@@ -89,10 +89,31 @@ wordcloud(rownames(freq), freq[,1], max.words=200, colors=brewer.pal(1, "Dark2")
 #==============
 #topic clustering
 #==============
-
-
-
-
+# create tf-idf matrix
+tdm <- review_dtm
+term_tfidf <- tapply(tdm$v/row_sums(tdm)[tdm$i], tdm$j, mean) * log2(nDocs(tdm)/col_sums(tdm > 0))
+summary(term_tfidf)
+tdm <- tdm[,term_tfidf >= 0.1]
+tdm <- tdm[row_sums(tdm) > 0,]
+summary(col_sums(tdm))
+#Deciding best K value using Log-likelihood method
+best.model <- lapply(seq(2, 50, by = 1), function(d){LDA(tdm, d)})
+best.model.logLik <- as.data.frame(as.matrix(lapply(best.model, logLik)))
+#calculating LDA
+k = 50;#number of topics
+SEED = 7000;
+CSC_TM <-list(VEM = LDA(tdm, k = k, control = list(seed = SEED)),
+              VEM_fixed = LDA(tdm, k = k,control = list(estimate.alpha = FALSE, seed = SEED)),
+              Gibbs = LDA(tdm, k = k, method = 'Gibbs',control = list(seed = SEED, burnin = 1000,
+                                                                      thin = 100, iter = 1000)),
+              CTM = CTM(tdm, k = k,control = list(seed = SEED,var = list(tol = 10^-4), 
+                                                  em = list(tol = 10^-3))))
+#To compare the fitted models we first investigate the values of the models
+#fitted with VEM and estimated and with VEM and fixed 
+sapply(CSC_TM[1:2], slot, 'alpha')
+sapply(CSC_TM, function(x) mean(apply(posterior(x)$topics, 1, function(z) - sum(z * log(z)))))
+Topic <- topics(CSC_TM[['VEM']], 1)
+Terms <- terms(CSC_TM[['VEM']], 8)
 
 #==============
 #build classifier
