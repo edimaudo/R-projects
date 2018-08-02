7a5907abb9d71bfcf02b7f311986913a5e4a56de
Edima Udo <edimaudo@gmail.com>
2018-08-02 00:45:10 -0400

    solved the first two problems

--- R_analytics/travel/travel.R
+++ R_analytics/travel/travel.R
@@ -2,91 +2,167 @@
 #business background
 #-------------------
 # One very common marketing problem is where & when to invest in order to drive the most conversions with least cost. 
-# In our travel business, We are trying to figure out the same problem. In this challenge you will be given a sample data set. 
-# Each row represent a visit to our product, and each row has several columns consist of the behavioral & demographic features 
+# In our travel business, We are trying to figure out the same problem. 
+#In this challenge you will be given a sample data set. 
+# Each row represent a visit to our product, 
+#and each row has several columns consist of the behavioral & demographic features 
 # such as the route they search within this visit, the departure date and the marketing channel that the visit come from, 
 # as well as a column indicating how many conversions have happened(0 means no conversion happened).
 
 
 #business objective
+# 1) Please rank the importance of each feature based on their level of correlation to the happening of conversions
+# 2) Please identify for route JKT(Jakarta) - DPS(Bali), which channel x device x travel time x any 
+# other significant influencing feature we should invest in most in order to drive most bookings out of this route
+# 3) Please build a machine learning model that takes all columns (can also choose features) except “conversions” as x 
+#and “conversions” as y 
+#to predict whether a booking will happen given all column x
 
 #data dictionary
 
+# data_created_at: Time when the visit to our product(i.e. web, app) happens
+# 
+# device: The device that makes this visit, i.e. desktop, smartphone
+# 
+# Os_type: The operation of the visiting device, i.e. iOS, android, windows
+# 
+# Client_type: product type, either web or app
+# 
+# Airlines: The two letter code of the airlines that the user select to fly with. If it’s round trip, 
+#the outbound and inbound airlines are separated by “=”. 
+#If the user changes airline within any leg, 
+#(i.e. a user’s final destination is hong kong but stopover at KL and changes an airline), 
+#then the two segment airline would be separated by “-” 
+# 
+# Stops: With stopover or without stopover
+# 
+# Trip_type: oneway or roundtrip
+# 
+# Trip_category: domestic or international
+# 
+# Cabin_class: EONOMY, PREMIUM ECONOMY OR BUSINESS
+# 
+# Adults_count: Number of adults
+# 
+# Children_count: Number of children
+# 
+# Infants_count: Number of infants
+# 
+# Total_price_usd: Total value of tickets that the user select, with 5 decimal points
+# 
+# User_city: Where the user is when he/she conducts the product visit
+# 
+# Channel: The marketing channel that the user come from
+# 
+# Device_resolution: i.e. 1080 x 1794
+# 
+# Device_version: Model of the visiting device i.e. Samsung G930F
+# 
+# Os_version: Operating system of the device i.e. iOS 8.0
+# 
+# App_version: app version, the value is null if client type = web
+# 
+# Network_type: 4g, wifi or 3g
+# 
+# Network_carrier_name: Telecom company that hosts the internet connection, i.e. Singtel
+# 
+# Conversions: Number of goal happened
+
 #remove old data
+rm(list=ls())
 
 #load libraries
+for (package in c('ggplot2', 'corrplot','tidyverse',
+                  "cowplot",'lubridate','data.table','car','caret','mlbench','xgboost','plotrix')) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
 
 #load data
+df <- read.csv(file.choose())
 
 #backup data
+df.backup <- df
 
 #review data
+print(summary(df))
 
-#exploratory analysis
-
-#data transformation
-
-#split data
-
-#modelling
-
-#check for features
-
-#
+#missing data
+missing_data <- apply(df, 2, function(x) any(is.na(x)))
+print(missing_data) #no missing data
 
 
+#1) 
+#look for significant correlations
+corrinfo <- df %>%
+  select(conversions,adults_count, children_count, infants_count,total_price_usd)
+corrplot(cor(corrinfo), method = "number")
+#none of the numerical values has any strong correlation
 
-Please try to answer the following questions:
-1) Please rank the importance of each feature based on their level of correlation to the happening of conversions
-2) Please identify for route JKT(Jakarta) - DPS(Bali), which channel x device x travel time x any other significant influencing feature we should invest in most in order to drive most bookings out of this route
-3) Please build a machine learning model that takes all columns except “conversions” as x and “conversions” as y to predict whether a booking will happen given all column x
+#2)
+# Please identify for route JKT(Jakarta) - DPS(Bali), which channel x device x travel time x any 
+# other significant influencing feature we should invest in most in order to drive most bookings out of this route
 
-Guide
-1) Please explain the rationales behind using a particular statistical/ML model for your solution, and why that model is better than other potential models (even if you just do a excel pivot analysis)
-2) You may choose to ignore certain features, and if you do, please explain why you ignore those features
-3) Please feel free to create additional features or perform any feature transformation for the data. Note that the data might not be 100% clean.
+routeJKTDSP <- df %>%
+  filter(departure_city_code == "JKT", arrival_city_code == "DPS")
 
-Column explanation:
-  data_created_at: Time when the visit to our product(i.e. web, app) happens
+routeinfo <- routeJKTDSP %>%
+  select(conversions, device, os_type, client_type, channel, device_version, app_version)
 
-device: The device that makes this visit, i.e. desktop, smartphone
+#library(MASS)
+#scatterplot.matrix(routeinfo)
 
-Os_type: The operation of the visiting device, i.e. iOS, android, windows
+#device vs conversions
+ggplot(data = df,aes(x=as.factor(device),y=conversions)) + geom_bar(stat="identity") + 
+  theme_classic() +  theme(axis.text.x = element_text(angle=60, hjust=1)) +
+  xlab("device") + ylab("Conversions") #smartphone had the highest conversion numbers
 
-Client_type: product type, either web or app
+#os type vs conversions
+ggplot(data = df,aes(x=as.factor(os_type),y=conversions)) + geom_bar(stat="identity") + 
+  theme_classic() +  theme(axis.text.x = element_text(angle=60, hjust=1)) +
+  xlab("os type") + ylab("Conversions") # android and ios had the highest conversions which is in line with device info above
 
-Airlines: The two letter code of the airlines that the user select to fly with. If it’s round trip, the outbound and inbound airlines are separated by “=”. If the user changes airline within any leg, (i.e. a user’s final destination is hong kong but stopover at KL and changes an airline), then the two segment airline would be separated by “-” 
+#client type vs conversions
+ggplot(data = df,aes(x=as.factor(client_type),y=conversions)) + geom_bar(stat="identity") + 
+  theme_classic() +  theme(axis.text.x = element_text(angle=60, hjust=1)) +
+  xlab("Client type") + ylab("Conversions") #inline with os and device type
 
-Stops: With stopover or without stopover
+#app versions vs. conversions
+ggplot(data = df,aes(x=as.factor(app_version),y=conversions)) + geom_bar(stat="identity") + 
+  theme_classic() +  theme(axis.text.x = element_text(angle=60, hjust=1)) +
+  xlab("app versions") + ylab("Conversions") #app versions 5.5.2 and 5.6.9 had very high conversions
 
-Trip_type: oneway or roundtrip
+#channel type vs. conversions
+ggplot(data = df,aes(x=as.factor(channel),y=conversions)) + geom_bar(stat="identity") + 
+  theme_classic() +  theme(axis.text.x = element_text(angle=60, hjust=1)) +
+  xlab("Channel") + ylab("Conversions") #mobile app had the highest conversion
 
-Trip_category: domestic or international
+#mobile users have the highest conversion rate
 
-Cabin_class: EONOMY, PREMIUM ECONOMY OR BUSINESS
+#3 build predictive model
+#data modelling
 
-Adults_count: Number of adults
+#remove nas
+#drop date created at
+#drop depature airport code
+#drop departure airport code
+#drop arrival airport code
+#drop arrival country code
+#drop outbound date
+#drop inbound date
 
-Children_count: Number of children
+#transform categorical variables and remove core value
 
-Infants_count: Number of infants
+#split data in test and train
 
-Total_price_usd: Total value of tickets that the user select, with 5 decimal points
+#check for main features
 
-User_city: Where the user is when he/she conducts the product visit
+#try multiple machine learning models for binary classification
 
-Channel: The marketing channel that the user come from
+#see accuracy
 
-Device_resolution: i.e. 1080 x 1794
+#use on test model
 
-Device_version: Model of the visiting device i.e. Samsung G930F
-
-Os_version: Operating system of the device i.e. iOS 8.0
-
-App_version: app version, the value is null if client type = web
-
-Network_type: 4g, wifi or 3g
-
-Network_carrier_name: Telecom company that hosts the internet connection, i.e. Singtel
-
-Conversions: Number of goal happened
+#see accuracy
