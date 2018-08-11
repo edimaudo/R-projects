13657f9ebb2e5976e2b4d0921c7d3524600caf4d
Edima Udo <edimaudo@gmail.com>
2018-08-10 21:08:24 -0400

    built preliminary model

--- R_analytics/heart/heart.R
+++ R_analytics/heart/heart.R
@@ -71,3 +71,87 @@ names(df) <- c('age','sex','chest_pain_type','resting_blood_pressure',"serum_cho
 ggplot(data=df, aes(x=factor(disease))) +
   geom_bar() + theme_classic() + xlab("Number of heart diseases")  
 
+df.backup <- df
+
+df <- df.backup
+
+df <- df %>% 
+  select('age', 
+         'sex', 
+         'max_heart', 
+         'resting_blood_pressure', 'disease')
+
+
+df$disease <- recode_factor(df$disease, "1" = "0", "2" = "1")
+
+df <- na.omit(df)
+
+#split data into train and test
+library(caTools)
+set.seed(123)
+
+sample <- sample.split(df,SplitRatio = 0.75)
+train <- subset(df,sample ==TRUE)
+test <- subset(df, sample==FALSE)
+
+library(car)
+scatterplotMatrix(df[,1:4])
+
+library( 'e1071' )
+model <- svm(disease~., train)
+res <- predict( model, newdata=train )
+res1 <- predict( model, newdata=test )
+
+res.new <- as.data.frame(res)
+res1.new <- as.data.frame(res1)
+
+library(SDMTools)
+confusion.matrix((train$disease), (res.new$res), threshold = 0.5)
+accuracy(train$diease, res.new, threshold = 0.5)
+
+#confusion matrix
+table(train$disease, res.new$res)
+table(test$diease, res1.new$res1)
+
+
+#find important columns
+#names(getModelInfo())
+
+#Application of a Machine Learning Algorithm to training dataset
+# model_gbm<-train(trainSet[,predictors],trainSet[,outcomeName],method='gbm')
+# model_rf<-train(trainSet[,predictors],trainSet[,outcomeName],method='rf')
+# model_nnet<-train(trainSet[,predictors],trainSet[,outcomeName],method='nnet')
+# model_glm<-train(trainSet[,predictors],trainSet[,outcomeName],method='glm')
+
+#Setup a ML algorithm and parameter settings
+# fitControl <- trainControl(
+#   method = "repeatedcv",
+#   number = 5,
+#   repeats = 5)
+
+#remember to add verbose=FALSE to avoid endless outputs
+
+#fine tune model
+#modelLookup(model='gbm')
+#Creating grid
+#grid <- expand.grid(n.trees=c(10,20,50,100,500,1000),shrinkage=c(0.01,0.05,0.1,0.5),n.minobsinnode = c(3,5,10),
+interaction.depth=c(1,5,10))
+# training the model
+#model_gbm<-train(trainSet[,predictors],trainSet[,outcomeName],method='gbm',trControl=fitControl,tuneGrid=grid)
+# summarizing the model
+#print(model_gbm)
+
+#using tune length
+#using tune length
+#model_gbm<-train(trainSet[,predictors],trainSet[,outcomeName],method='gbm',trControl=fitControl,tuneLength=10)
+#print(model_gbm)
+
+#Checking variable importance for GBM
+#Variable Importance
+#varImp(object=model_gbm)
+
+#Evaluation of trained Algorithm (or Model) and result
+#Predictions
+#predictions<-predict.train(object=model_gbm,testSet[,predictors],type="raw")
+#table(predictions)
+#confusionMatrix(predictions,testSet[,outcomeName])
