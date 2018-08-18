4e4f7918a1d15758feea42a64f60526f848482de
Edima Udo <edimaudo@gmail.com>
2018-08-18 18:56:29 -0400

    laid out bank analysis steps, data information and objectives

---
+++ R_analytics/bank/bank.R
@@ -0,0 +1,74 @@
+# #bank data
+# 1. Title: Bank Marketing
+# 
+# 2. Relevant Information:
+#   
+# The data is related with direct marketing campaigns of a Portuguese banking institution. 
+# The marketing campaigns were based on phone calls. Often, 
+# more than one contact to the same client was required.
+# 
+# 
+# 3. Number of Instances: 45211 for bank-full.csv (4521 for bank.csv)
+# 
+# 4. Number of Attributes: 16 + output attribute.
+# 
+# 5. Attribute information:
+#   Input variables:
+#   # bank client data:
+#   1 - age (numeric)
+# 2 - job : type of job (categorical: "admin.","unknown","unemployed","management","housemaid","entrepreneur","student",
+#                        "blue-collar","self-employed","retired","technician","services") 
+# 3 - marital : marital status (categorical: "married","divorced","single"; note: "divorced" means divorced or widowed)
+# 4 - education (categorical: "unknown","secondary","primary","tertiary")
+# 5 - default: has credit in default? (binary: "yes","no")
+# 6 - balance: average yearly balance, in euros (numeric) 
+# 7 - housing: has housing loan? (binary: "yes","no")
+# 8 - loan: has personal loan? (binary: "yes","no")
+# # related with the last contact of the current campaign:
+# 9 - contact: contact communication type (categorical: "unknown","telephone","cellular") 
+# 10 - day: last contact day of the month (numeric)
+# 11 - month: last contact month of year (categorical: "jan", "feb", "mar", ..., "nov", "dec")
+# 12 - duration: last contact duration, in seconds (numeric)
+# # other attributes:
+# 13 - campaign: number of contacts performed during this campaign and for this client (numeric, includes last contact)
+# 14 - pdays: number of days that passed by after the client was last contacted from a previous campaign (numeric, -1 means client was not previously contacted)
+# 15 - previous: number of contacts performed before this campaign and for this client (numeric)
+# 16 - poutcome: outcome of the previous marketing campaign. Not an indication of purchase, 
+# contact showed interest in product (categorical: "unknown","other","failure","success")
+# Output variable (desired target):
+# 17 - y - has the client subscribed a term deposit? (binary: "yes","no")
+# 
+# 6. Missing Attribute Values: None
+# 
+# 7. Objective
+# Design a predictive models that would help in designing 
+# recommendation on how to optimize targeting moving forward, assuming these same data points 
+# will be available. 
+
+#remove old data
+rm(list=ls())
+
+#load libraries
+for (package in c('ggplot2', 'corrplot','tidyverse','caret','mlbench','Hmisc')) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+df <- read.csv(file.choose())
+
+summary(df)
+
+#check data balance
+
+#data cleaning
+
+#data transformation
+
+#find which features are important
+
+#split data into train and test
+
+#design model
+
