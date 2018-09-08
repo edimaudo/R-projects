14e54bd7eb5615a25dd20cfbe26224b429e28bf7
Edima Udo <edimaudo@gmail.com>
2018-09-08 19:45:53 -0400

    split data into train and test

--- R_analytics/churn_telco/churn_telco.R
+++ R_analytics/churn_telco/churn_telco.R
@@ -37,11 +37,57 @@ df$Phone <- NULL
 
 glimpse(df)
 
-#split columns into categorical and cts data
+#split columns into categorical and cts data and predicted variable
 df_category <- df %>%
   select(State, `Area Code`,`Int'l Plan`,`VMail Plan`)
 
+#continuous
+df_continuous <- select(df, -col(df_category))
+df_continuous$`VMail Plan` <- NULL
+df_continuous$`Churn?` <- NULL
 
-df_cts <- select(df, -col(df_category))
-df_churn <- df_cts$`Churn?` 
-df_cts$`Churn?` <- NULL
+#normalize data
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+df_continuous <- as.data.frame(lapply(df_continuous, normalize))
+
+#update column names
+df_category <- df_category %>%
+  rename(Area_Code = `Area Code`, Intl_Plan = `Int'l Plan`, VMail_Plan = `VMail Plan`) %>%
+  mutate(State = as.factor(State), Area_Code = as.factor(Area_Code), 
+         Intl_Plan = as.factor(Intl_Plan), VMail_Plan = as.factor(VMail_Plan))
+
+library(dummies)
+df_category.new <- dummy.data.frame(as.data.frame(df_category), sep = "_")
+
+#churn column
+df_churn <- df$`Churn?`
+
+#combine data
+df_combine <- cbind(df_continuous, df_category.new, df_churn)
+
+#find which features are not important
+set.seed(1)
+# # calculate correlation matrix
+correlationMatrix <- cor(df_combine[,1:72])
+# # summarize the correlation matrix
+# print(correlationMatrix)
+# # find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# # print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+#drop columns
+df_combine <- df_combine[,-c(highlyCorrelated)]
+
+#build model
+df_combine$df_churn <- recode_factor(df_combine$df_churn, "False." = "0", "True." = "1")
+
+#split data into train and test
+set.seed(1)
+library(caTools)
+sample <- sample.split(df_combine,SplitRatio = 0.75)
+train <- subset(df_combine,sample ==TRUE)
+test <- subset(df_combine, sample==FALSE)
