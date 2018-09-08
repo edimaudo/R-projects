1f2b77d018ea04dd665e5a21cbc918ccbe0c6a4e
Edima Udo <edimaudo@gmail.com>
2018-09-08 15:35:18 -0400

    update

--- R_analytics/churn_telco/churn_telco.R
+++ R_analytics/churn_telco/churn_telco.R
@@ -27,8 +27,28 @@ missing_data <- apply(df, 2, function(x) any(is.na(x))) #no missing data
 print(missing_data)
 
 #check for balanced data
-ggplot(data=df, aes(x=factor(Subscription))) +
-  geom_bar() + theme_classic() + xlab("Type of subscription")
+ggplot(data=df, aes(x=factor(`Churn?`))) +
+  geom_bar() + theme_classic() + xlab("Churn Amount") #data is imbalanced
 
-
+#TRY data balancing - SKIP DATA BALANCE
+# 
+# library(unbalanced)
+# 
+# n<-ncol(df)
+# output<-df$`Churn?`
+# output<-as.factor(output)
+# input<-  df[ ,-n]
+# #View(input)
+# 
+# #Balance the Dataset using ubSMOTE#
+# data<-ubBalance(X= input, Y=output, type="ubSMOTE", percOver=200, percUnder=200, k = 5,verbose=TRUE)
+# #View(data)
+#                 
+# #Balanced Data#
+# balancedData<-cbind(data$X,data$Y)
+# #View(balancedData)
+# 
+# #check data balance again
+# ggplot(data=balancedData, aes(x=factor(`data$Y`))) +
+#   geom_bar() + theme_classic() + xlab("Churn Amount")
 
