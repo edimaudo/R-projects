ba8cc243665f69ca29f29867b875b9d43f62bb5e
Edima Udo <edimaudo@gmail.com>
2018-09-14 18:03:38 -0400

    recommendations

--- shiny/recommendations/app.R
+++ shiny/recommendations/app.R
@@ -1,50 +1,100 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#load libraries
 library(shiny)
+require(shinydashboard)
+library(ggplot2)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+#load data
+recommendation <- read.csv('recommendation.csv',stringsAsFactors = F,header=T)
+#head(recommendation)
+
+ui <- dashboardPage(
+  #dashboardHeader(),
+  #dashboardSidebar(),
+  #dashboardBody()
+  #Dashboard header carrying the title of the dashboard
+  header <- dashboardHeader(title = "Basic Dashboard"),  
+  #Sidebar content of the dashboard
+  sidebar <- dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Visit-us", icon = icon("send",lib='glyphicon'), 
+               href = "https://www.salesforce.com")
+    )
+  ),
+  frow1 <- fluidRow(
+    valueBoxOutput("value1")
+    ,valueBoxOutput("value2")
+    ,valueBoxOutput("value3")
+  ),
+  frow2 <- fluidRow( 
+    box(
+      title = "Revenue per Account"
+      ,status = "primary"
+      ,solidHeader = TRUE 
+      ,collapsible = TRUE 
+      ,plotOutput("revenuebyPrd", height = "300px")
+    )
+    ,box(
+      title = "Revenue per Product"
+      ,status = "primary"
+      ,solidHeader = TRUE 
+      ,collapsible = TRUE 
+      ,plotOutput("revenuebyRegion", height = "300px")
+    ) 
+  ),
+  # combine the two fluid rows to make the body
+  body <- dashboardBody(frow1, frow2),
+  #completing the ui part with dashboardPage
+  ui <- dashboardPage(title = 'This is my Page title', header, sidebar, body, skin='red')
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+# create the server functions for the dashboard  
+server <- function(input, output) { 
+  #some data manipulation to derive the values of KPI boxes
+  total.revenue <- sum(recommendation$Revenue)
+  sales.account <- recommendation %>% group_by(Account) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  prof.prod <- recommendation %>% group_by(Product) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  #creating the valueBoxOutput content
+  output$value1 <- renderValueBox({
+    valueBox(
+      formatC(sales.account$value, format="d", big.mark=',')
+      ,paste('Top Account:',sales.account$Account)
+      ,icon = icon("stats",lib='glyphicon')
+      ,color = "purple")  
+  })
+  output$value2 <- renderValueBox({ 
+    valueBox(
+      formatC(total.revenue, format="d", big.mark=',')
+      ,'Total Expected Revenue'
+      ,icon = icon("gbp",lib='glyphicon')
+      ,color = "green")  
+  })
+  output$value3 <- renderValueBox({
+    valueBox(
+      formatC(prof.prod$value, format="d", big.mark=',')
+      ,paste('Top Product:',prof.prod$Product)
+      ,icon = icon("menu-hamburger",lib='glyphicon')
+      ,color = "yellow")   
+  })
+  #creating the plotOutput content
+  output$revenuebyPrd <- renderPlot({
+    ggplot(data = recommendation, 
+           aes(x=Product, y=Revenue, fill=factor(Region))) + 
+      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
+      xlab("Product") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Revenue by Product") + labs(fill = "Region")
+  })
+  output$revenuebyRegion <- renderPlot({
+    ggplot(data = recommendation, 
+           aes(x=Account, y=Revenue, fill=factor(Region))) + 
+      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
+      xlab("Account") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Revenue by Region") + labs(fill = "Region")
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
