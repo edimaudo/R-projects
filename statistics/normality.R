b3edc3fa3c4d46e6ddada9f32bab12e2a5a49411
Edima Udo <edimaudo@gmail.com>
2018-09-17 17:57:27 -0400

    created normality file

---
+++ statistics/normality.R
@@ -0,0 +1,8 @@
+#clear old data
+rm(list=ls())
+
+library(readxl)         # read in Excel data
+library(ggplot2)        # create visualizations
+library(psych)          # provide summary statistics
+library(pastecs)        # provide summary statistics
+
