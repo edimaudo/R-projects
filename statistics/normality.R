b5f8e7cbac4af587d4f4ecf28722fbbb539c55b8
Edima Udo <edimaudo@gmail.com>
2018-09-18 18:07:59 -0400

    update

--- statistics/normality.R
+++ statistics/normality.R
@@ -5,4 +5,6 @@ library(readxl)         # read in Excel data
 library(ggplot2)        # create visualizations
 library(psych)          # provide summary statistics
 library(pastecs)        # provide summary statistics
+library(tidyverse)
 
+golf <- read_excel(file.choose(), sheet = "2011") #golf data
