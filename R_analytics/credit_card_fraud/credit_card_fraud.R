657202bc3f9dadfaa5380296a17315214d5bdd9c
Edima Udo <edimaudo@gmail.com>
2018-09-30 14:07:43 -0400

    credit fraud modelling
    loaded data and checked for class information

---
+++ R_analytics/credit_card_fraud/credit_card_fraud.R
@@ -0,0 +1,40 @@
+
+# Fraud Model - These models are being used to know if a particular transaction 
+# is a fraudulent transaction. Historical data having details of fraud and non-fraud 
+# transactions can be used to build a classification model that would predict chances of 
+# fraud happening in a transaction.
+
+
+#remove old data
+rm(list=ls())
+
+#load libraries
+for (package in c('ggplot2', 'corrplot','tidyverse','caret','mlbench', 'xgboost', 'caTools')) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
+df <- read.csv(file.choose())
+
+df.backup <- df
+
+#summary 
+print(summary(df))
+
+#check for missing data
+missing_data <- apply(df, 2, function(x) any(is.na(x))) #no missing data
+print(missing_data)
+
+#check class
+ggplot(data=df, aes(x=factor(Class))) +
+  geom_bar() + theme_classic() + xlab("Class Information")
+
+#check for imbalanced data
+
+
+#data transformation
+
+#data modelling
