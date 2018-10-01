73cec9f5cef29c3f548ffe56061d26f037f74bd3
Edima Udo <edimaudo@gmail.com>
2018-10-01 06:34:28 -0400

    created retail sales file

---
+++ R_analytics/retail_sales/retail_sales.R
@@ -0,0 +1,11 @@
+# Context
+# This dataset contains lot of historical sales data. 
+# It was extracted from a Brazilian top retailer and has many SKUs and many stores. 
+# The data was transformed to protect the identity of the retailer.
+
+# Current inventory management models have many solutions to place the correct order,
+# but they are all based in a single unknown factor: the demand for the next periods.
+# This is why short-term forecasting is so important in retail and consumer goods industry.
+# We encourage you to seek for the best demand forecasting model for the next 2-3 weeks. 
+# This valuable insight can help many supply chain practitioners 
+# to correctly manage their inventory levels.
