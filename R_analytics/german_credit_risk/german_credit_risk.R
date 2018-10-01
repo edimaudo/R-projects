20b0a18f5f55289f491ad2bc403bd96144fb6dbc
Edima Udo <edimaudo@gmail.com>
2018-10-01 08:19:57 -0400

    update
    Use mice to fix missing data, recoded continuous and categorical variables, performed 1 hot encoding for categorical variables

--- R_analytics/german_credit_risk/german_credit_risk.R
+++ R_analytics/german_credit_risk/german_credit_risk.R
@@ -25,15 +25,38 @@ df.backup <- df
 print(summary(df))
 
 #look for missing data
+missing_data <- apply(df, 2, function(x) any(is.na(x)))
+print(missing_data) #savings and check account have missing data
+
+df1 <- na.omit(df)
 
 #fix missing data
+colnames(df) <- c('Age','Sex','Job','Housing','Saving_Account','Checking_Account','Credit_Amount','Duration',
+             'Purpose','Risk')
+
+df_update <- mice(df)
+df_update<-complete(df_update)
+df <- df_update
 
 #look at risk profile
+ggplot(data=df, aes(x=factor(Risk))) +
+  geom_bar() + theme_classic() + xlab("Risk information")
+
+#recode variables
+df_cts <- df %>%
+  select(Age,Credit_Amount, Duration)
+
+df_cts <- as.data.frame(lapply(df_cts, normalize))
+
+df_categorical <- df %>%
+  select(Sex, Job, Housing, Saving_Account, Checking_Account, Purpose)
+
+library(dummies)
+df_category.new <- dummy.data.frame(as.data.frame(df_categorical), sep = "_")
+
+df_final <- cbind(df_category.new, df_cts, df$Risk)
 
 #recode risk profile
-
-#remove redundant columns
-
 
 
 #create models
