a62b612e58421e93623859031b739b1af0620b1f
Edima Udo <edimaudo@gmail.com>
2018-10-01 06:38:29 -0400

    updated german credit risk file with steps to take

---
+++ R_analytics/german_credit_risk/german_credit_risk.R
@@ -0,0 +1,39 @@
+#remove old data
+rm(list=ls())
+
+#load libraries
+for (package in c('ggplot2', 'corrplot','tidyverse','caret','mlbench')) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#normalize data
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+#load data
+df <- read_csv(file.choose())
+
+#create backup
+
+#summary
+
+#look for missing data
+
+#fix missing data
+
+#look at risk profile
+
+#recode risk profile
+
+
+
+#create models
+
+library(randomForest)
+
+library(gbm)
+
