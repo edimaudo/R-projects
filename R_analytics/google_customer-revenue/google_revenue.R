293a0e4b5bd4f2ee52de39a3f810dc1fe7e141b9
Edima Udo <edimaudo@gmail.com>
2018-10-08 14:12:57 -0400

    converted date column from int to date

--- R_analytics/google_customer-revenue/google_revenue.R
+++ R_analytics/google_customer-revenue/google_revenue.R
@@ -3,7 +3,7 @@ rm(list=ls())
 
 #load libraries
 packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools', 
-              'MASS','Metrics','randomForest','lars','xgboost','Matrix','methods', 'data.table')
+              'MASS','Metrics','randomForest','lars','xgboost','Matrix','methods', 'data.table', 'lubridate')
 
 #load libraries
 for (package in packages) {
@@ -16,9 +16,25 @@ for (package in packages) {
 #load data
 train <- fread(file.choose())
 
+#get summary
+print(summary(train))
 
+#check for missing data
+missing_data <- apply(train, 2, function(x) any(is.na(x)))
+print(missing_data) #no missing data
 
+glimpse(train)
 
+#convert date column to real date
+train$date <- ymd(train$date)
+
+#convert visit start time
+
+#extract data from total column and create new columns
+
+#extract data from geonetwork columns and create new columns
+
+#extract data from device column and create new columns
 
 #load test data
 test <- fread(file.choose())
