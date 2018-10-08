d5d87adbd3aaa9c91f4732d6512cc6f4b8a4f23f
Edima Udo <edimaudo@gmail.com>
2018-10-08 14:00:06 -0400

    loaded libraries and data

---
+++ R_analytics/google_customer-revenue/google_revenue.R
@@ -0,0 +1,27 @@
+#remove old data
+rm(list=ls())
+
+#load libraries
+packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools', 
+              'MASS','Metrics','randomForest','lars','xgboost','Matrix','methods', 'data.table')
+
+#load libraries
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
+train <- fread(file.choose())
+
+
+
+
+
+#load test data
+test <- fread(file.choose())
+
+#load sample submission
+sample_submission <- read_csv(file.choose())
