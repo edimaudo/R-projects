7ccba1be80d518f5e1df2108cec987363cbf1f6c
Edima Udo <edimaudo@gmail.com>
2018-10-11 18:09:25 -0400

    update
    Split data into category and continuous.  Removed extra columns after one hot encoding.  Split data into train and test

--- R_analytics/diamond_pricing/diamond_pricing.R
+++ R_analytics/diamond_pricing/diamond_pricing.R
@@ -25,16 +25,39 @@ print(summary(df))
 missing_data <- apply(df, 2, function(x) any(is.na(x)))
 print(missing_data)
 
-#check for missing data
-
-
-
+#rename columns
+colnames(df) <- c('Color','Clarity','Weight',"Price")
 
 #normalize data
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
+#modelling
+#split data into category and cts
 
+df_category <- df %>%
+  dplyr::select(Color,Clarity)
+
+library(dummies)
+df_category.new <- dummy.data.frame(as.data.frame(df_category), sep = "_")
+df_category.new <- as.data.frame(df_category.new)
+
+glimpse(df_category.new)
+
+#drop information to prevent multcollineariy
+df_category.new[1] <- NULL
+df_category.new[28] <- NULL
+
+df$Weight <- normalize(df$Weight)
+df_final <- cbind(df_category.new, df[,3], df[,4])
+
+#regression modelling
+
+#split data into train and test
+set.seed(123)
+sample <- sample.split(df_final,SplitRatio = 0.75)
+training <- subset(df_final,sample ==TRUE)
+test <- subset(df_final, sample==FALSE)
 
 
