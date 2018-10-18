c42527e3d238703089aab17e328b4ed4bd42642f
Edima Udo <edimaudo@gmail.com>
2018-10-18 17:44:33 -0400

    added code to clean string and add to data frame

--- R_analytics/google_customer-revenue/google_revenue.R
+++ R_analytics/google_customer-revenue/google_revenue.R
@@ -32,6 +32,7 @@ train$date <- ymd(train$date)
 #convert visit start time
 train$visitStartTime <- as.POSIXct(train$visitStartTime, origin="1970-01-01")
 
+#function to clean total column
 Clean_String <- function(extransaction){
   temp <- extransaction
   #' Remove everything that is not a number or letter 
@@ -41,14 +42,62 @@ Clean_String <- function(extransaction){
   # Shrink down to just one white space
   temp <- stringr::str_replace_all(temp,"[\\s]+", " ")
   return (temp)
-
 }
 
+#extract data from totals column
+df_train_info <- data.frame()
+n <- nrow(train)
+for (i in 1:n){
+  train_info <- stringr::str_split_fixed(Clean_String(train$totals[i])," ", 5)
+  df_train_info <- rbind(df_train_info,train_info)
+}
 
+#rename df_train_info column
+colnames(df_train_info) <- c("visits", "hits","pageviews", "bounces","newvisits")
+
+#extract data from traffic source
+
+#extract data from geonetwork columns and create new columns
+
+#extract data from device column and create new columns
+
+#combine data from new data frames
+
+#drop totals, traffic, geonetwork, device columns
+
+#create dummy variables
+
+#normalize data
+
+#drop core dummy variables to prevent mullticollinearity
+
+#load test data
+test <- fread(file.choose())
+
+#recode test data similar to train data
+
+#create model
+
+#test model
+
+#load sample submission
+sample_submission <- read_csv(file.choose())
+
+#update submission using new model
+
+#tweak model based on ranking
+
+
+#======================others
 #string split
 #stringr::str_split_fixed(x$y, "", 4)
-#df %>% separate(file_name, c("ID", "Month","Year", "rest"))
+#df %>% separate(Clean_String(train$totals[5]), c("visits", "hits","pageviews", "bounces","newvisits"))
 #Separate(data,column,into=” “,sep=” ” )
+#train$visits <- 
+#train$hits <-
+#train$pageviews <-
+#train$bounces <-
+#train$newVisits <-
 
 # model <- #some processing
 #   df <- data.frame(col1=model)
@@ -73,31 +122,3 @@ Clean_String <- function(extransaction){
 #   
 # }
 # 
-# test2 <- stringr::str_split_fixed(test," ", 5)
-
-train_totals_update <- df_total
-colnames(train_totals_update)
-
-
-#extract data from total column and create new columns
-#train$visits <- 
-#train$hits <-
-#train$pageviews <-
-#train$bounces <-
-#train$newVisits <-
-
-#extract data from traffic source
-
-#extract data from geonetwork columns and create new columns
-
-#extract data from device column and create new columns
-
-#create model
-
-#load test data
-test <- fread(file.choose())
-
-#modify test data
-
-#load sample submission
-sample_submission <- read_csv(file.choose())
