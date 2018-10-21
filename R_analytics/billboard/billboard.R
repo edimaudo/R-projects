853b2adaf14dd153f75a78e5bade30e2e7709597
Edima Udo <edimaudo@gmail.com>
2018-10-21 18:36:44 -0400

    added billboard song word count

--- R_analytics/billboard/billboard.R
+++ R_analytics/billboard/billboard.R
@@ -27,6 +27,7 @@ print(missing_data)
 
 glimpse(df$Song)
 
+#song mentions
 song_mentions <- function(search_word, df){
   toMatch <-  c(search_word)
   count_val <- data.frame(string=df$Song, total_count=rowSums(sapply(toMatch, function(x) grepl(x, df$Song))))
@@ -35,8 +36,35 @@ song_mentions <- function(search_word, df){
   return (final_count$n[2])
 }
 
-#song mentions
 paste("Life shows up ", song_mentions("life",df), " times")
 paste("war shows up ", song_mentions("war",df), " times")
 paste("love shows up ", song_mentions("love",df), " times")
 
+#enter a keyword and return get count of billboard songs with the keyword
+
+billboard_songs <- function(search_word, df){
+  search_word <- sapply(search_word, tolower)
+  toMatch <-  c(search_word)
+  count_val <- data.frame(string=df$Song, total_count=rowSums(sapply(toMatch, function(x) grepl(x, df$Song))))
+  final_count <- count_val %>%
+    count(total_count)
+  output <- final_count$n[2]
+  if (output == 0){
+    return (paste("No songs were found to contain the word: ", search_word))
+  } else if (output == 1) {
+    return (paste(output ," song was found to have the phrase ' ", search_word, " ' in this dataset"))
+  } else {
+    return (paste(output ," songs were found to have the phrase ' ", search_word, " ' in this dataset"))
+  }
+}
+
+
+billboard_songs("like", df)
+
+
+#identify top words used in the top ten lyrics
+df_top_ten <- df[1:10,]
+df_top_ten_count <- data.frame(table(unlist(strsplit(tolower(df_top_ten$Lyrics), " "))))
+df_top_20_word_count <- df_top_ten_count %>%
+  arrange(desc(Freq)) %>%
+  top_n(20)
