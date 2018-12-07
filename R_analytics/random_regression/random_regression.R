3cd01fc45de5f8e63d935a4e996b83b063e2b3a1
Edima Udo <edimaudo@gmail.com>
2018-12-06 22:47:32 -0500

    Update random_regression.R

--- R_analytics/random_regression/random_regression.R
+++ R_analytics/random_regression/random_regression.R
@@ -51,10 +51,8 @@ R2=summary(fit)$r.squared
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
-#option 2 - use normalized data
+#option 2 - use normalized data + all variables
 #normalize data
-
-
 df_update <- normalize(df[,1:15])
 df_update <- cbind(df_update, df$Target)
 
@@ -63,7 +61,6 @@ sample <- sample.split(df,SplitRatio = 0.75)
 training <- subset(df,sample ==TRUE)
 test <- subset(df, sample==FALSE)
 
-#option 1 - use all variables
 fit <- lm(df$Target ~., data=training)
 summary(fit)
 AIC(fit)
@@ -71,13 +68,63 @@ plot(Target ~., data=training)
 #plot(residuals(fit) ~ ., data=training)
 #get accuracy and MSE
 pred1 <- predict(fit, newdata = test)
+
 error <- pred1 - test$Target
 MAE <- mae(error)
 R2=summary(fit)$r.squared
 
 #option 3 - normalize + feature importance
+#normalize data
+df_update <- normalize(df[,1:15])
+df_update <- cbind(df_update, df$Target)
 
-#option 4 - normalize + log + feature importance
+#find important features
+# # calculate correlation matrix
+correlationMatrix <- cor(df_update[,1:15])
+# # summarize the correlation matrix
+# print(correlationMatrix)
+# # find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# # print indexes of highly correlated attributes
+print(highlyCorrelated) #nothing highly correlated
+#no sepcial correlation
 
+#option - normalize + log
+df_update <- normalize(df[,1:15])
+df_update <- cbind(df_update, df$Target)
+
+#split data
+sample <- sample.split(df,SplitRatio = 0.75)
+training <- subset(df,sample ==TRUE)
+test <- subset(df, sample==FALSE)
+
+fit <- lm(log(df$Target) ~., data=training)
+summary(fit)
+AIC(fit)
+#plot(Target ~., data=training)
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$Target
+MAE <- mae(error)
+R2=summary(fit)$r.squared
+#-----------------------------------------
+fit <- lm(df$Target ~., data=log(training))
+summary(fit)
+AIC(fit)
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$Target
+MAE <- mae(error)
+R2=summary(fit)$r.squared
+#-------------------------------------------
+
+fit <- lm(log(df$Target) ~., data=log(training))
+summary(fit)
+AIC(fit)
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$Target
+MAE <- mae(error)
+R2=summary(fit)$r.squared
 
 
