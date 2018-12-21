644adfc614f3d9b715236f8df0ea94406277596c
Edima Udo <edimaudo@gmail.com>
2018-12-21 06:24:08 -0500

    Update suspicious_auctions.R

--- R_analytics/suspicious_auctions.R
+++ R_analytics/suspicious_auctions.R
@@ -13,6 +13,30 @@ for (package in packages) {
 }
 
 #load data
-notification_final <- fread.csv(file.choose(), sep="#")
-protocols_final <- read.csv(file.choose())
+notification_final_mo <- fread(file.choose(), sep="#")
+notification_final_m <- fread(file.choose(), sep="#")
+notification_final_sp <- fread(file.choose(), sep="#")
+protocols_final_mo <- fread(file.choose(), sep="#")
+protocols_final_m <- fread(file.choose(), sep="#")
+protocols_final_sp <- fread(file.choose(), sep="#")
 
+fields_extract_not_zk <- function(df){
+  final_output <- df
+  final_output <- final_output %>%
+    select(notificationnumber,versionnumber,publishdate,customerrequirement_maxprice,
+           notificationcommission_p1date,notificationcommission_p2date)
+}
+
+notification_final_mo <- fields_extract_not_zk(notification_final_mo)
+notification_final_m <- fields_extract_not_zk(notification_final_m)
+notification_final_sp <- fields_extract_not_zk(notification_final_sp)
+
+#combine notification data
+
+#sort notification data
+
+#keeps unique observations
+
+#filter data
+
+#create new variable
