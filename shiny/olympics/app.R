62cb7513f88fec86a598d3e34e6f35891c3d517b
Edima Udo <edimaudo@gmail.com>
2018-12-23 02:54:36 -0500

    update

--- shiny/olympics/app.R
+++ shiny/olympics/app.R
@@ -1,50 +1,60 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#load libraries
+#packages
+packages <- c("tidyverse",'shiny','shinydashboard')
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+#load data
+file_path <- "Olympics.csv"
+olympics <- read.csv(file_path)
 
+country <- unique(olympics$Country)
+
+#filter for one country - #select country
+#df_filtered <- df %>%
+#  dplyr::filter(Country == "Norway")
+
+# Use a fluid Bootstrap layout
+ui <- fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Olympics by country"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("country", "Country:", 
+                  choices=colnames(country))
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("ageSex")  
+    )
+    
+  )
+)
+
+
+
+# Define a server for the Shiny app
+server <- function(output,input){
+
+    # Fill in the spot we created for a plot
+    output$ageSex <- renderPlot({
+      ggplot(data=olympics, aes(x=Age, na.rm=TRUE)) + geom_bar(aes(fill = Sex)) + xlab("Age") + 
+        ggtitle("Age Vs Sex") + theme_bw()
+
+    })
+  
+}
+
+shinyApp(ui,server)
