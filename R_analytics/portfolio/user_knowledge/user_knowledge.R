f6a9c2a5015f1497682fe73e3bba643695d8b0a9
Edima Udo <edimaudo@gmail.com>
2019-02-21 07:04:26 -0500

    Update user_knowledge.R
    build model using gradient boost

--- R_analytics/portfolio/user_knowledge/user_knowledge.R
+++ R_analytics/portfolio/user_knowledge/user_knowledge.R
@@ -18,3 +18,65 @@ glimpse(train)
 #summary statisitcs
 summary(train)
 
+#normalize
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+UNS_train <- train$UNS
+
+
+#recode
+UNS_train <- recode_factor(UNS_train, "very_low"="1", "Low"= "2", "Middle" = "3", "High"="4")
+df_train <- train[,1:5]
+UNS <- recode_factor(UNS, "Very Low"="1", "Low"= "2", "Middle" = "3", "High"="4")
+df_train <- as.data.frame(lapply(df_train, normalize))
+
+
+train <- cbind(df_train, UNS_train)
+
+
+resultdata <- function(control, train){
+  set.seed(123)
+  #decision trees
+  fit.cart <- train(class~., data=train, method="rpart", trControl=control)
+  #LDA
+  fit.lda <- train(class~., data=train, method="lda", trControl=control)
+  #svm
+  fit.svm <- train(class~., data=train, method="svmRadial", trControl=control)
+  #random forest
+  fit.rf <- train(class~., data=train, method="rf", trControl=control)
+  #bagged cart
+  fit.treebag <- train(class~., data=train, method="treebag", trControl=control)
+  #boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
+  fit.gbm <- train(class~., data=train, method="gbm", trControl=control)
+  
+  #------------------
+  #compare models
+  #------------------
+  results <- resamples(list(cart = fit.cart, lda = fit.lda,
+                            svm = fit.svm, randomforest = fit.rf, 
+                            baggedcart = fit.treebag, gradboost = fit.gbm))
+  
+  return (results)
+}
+
+#cross fold validation
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+results <- resultdata(control, train)
+
+summary(results)
+
+# boxplot comparison
+bwplot(results)
+# Dot-plot comparison
+dotplot(results)
+
+#use test data
+fit.gbm <- train(UNS_train~., data=train, method="gbm", trControl=control)
+UNS_test <- test$UNS
+df_test <- test[,1:5]
+df_test <- as.data.frame(lapply(df_test, normalize))
+test <- cbind(df_test, UNS_test)
+test_scores <- predict(fit.gbm, test)
+confusionMatrix(test_scores, test$UNS_test)
