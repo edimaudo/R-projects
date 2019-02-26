985f6b0c92b96f0930a96820f5e890aa45de9618
Edima Udo <edimaudo@gmail.com>
2019-02-25 22:14:54 -0500

    Update occupancy.R

--- R_analytics/portfolio/occupancy/occupancy.R
+++ R_analytics/portfolio/occupancy/occupancy.R
@@ -48,35 +48,12 @@ print(highlyCorrelated)
 train_new <- train_new[,-c(highlyCorrelated)]
 
 
-resultdata <- function(control, train){
-  set.seed(123)
-  #decision trees
-  fit.cart <- train(occupancy~., data=train, method="rpart", trControl=control)
-  #LDA
-  fit.lda <- train(occupancy~., data=train, method="lda", trControl=control)
-  #svm
-  fit.svm <- train(occupancy~., data=train, method="svmRadial", trControl=control)
-  #random forest
-  fit.rf <- train(occupancy~., data=train, method="rf", trControl=control)
-  #bagged cart
-  fit.treebag <- train(occupancy~., data=train, method="treebag", trControl=control)
-  #boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
-  fit.gbm <- train(occupancy~., data=train, method="gbm", trControl=control)
-  
-  #------------------
-  #compare models
-  #------------------
-  results <- resamples(list(cart = fit.cart, lda = fit.lda,
-                            svm = fit.svm, randomforest = fit.rf, 
-                            baggedcart = fit.treebag, gradboost = fit.gbm))
-  
-  return (results)
-}
-
 #cross fold validation
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
-results <- resultdata(control, train)
-
+#boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
+fit.gbm <- train(occupancy~., data=train, method="gbm", trControl=control)
+  
+results <- resamples(list(gradboost = fit.gbm))
 summary(results)
 
 # boxplot comparison
@@ -90,6 +67,5 @@ test_cts <- as.data.frame(lapply(test_cts, normalize))
 test_new <- test_new[,-c(highlyCorrelated)]
 occupancy <- as.factor(test$Occupancy)
 test_new <- cbind(test_cts, test_occupancy)
-fit.gbm <- train(occupancy~., data=train, method="gbm", trControl=control)
 test_scores <- predict(fit.gbm, test)
 confusionMatrix(test_scores, test$room)
