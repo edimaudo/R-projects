34a6a295d9352f096c50b96094d7734bda7765d9
Edima Udo <edimaudo@gmail.com>
2019-02-27 08:15:52 -0500

    Update order_data.R

--- R_analytics/portfolio/Orders_Data/order_data.R
+++ R_analytics/portfolio/Orders_Data/order_data.R
@@ -41,7 +41,7 @@ df_new <- cbind(df_cat_new, df_cts, Target)
 
 #fine tune model
 # # calculate correlation matrix
-correlationMatrix <- cor(df_new[,1:length(df_new)])
+correlationMatrix <- cor(df_new[,1:length(df_new)-1])
 # # summarize the correlation matrix
 # print(correlationMatrix)
 # # find attributes that are highly corrected (ideally >0.75)
@@ -57,3 +57,37 @@ sample <- sample.split(df_new,SplitRatio = 0.75)
 train <- subset(df_new,sample ==TRUE)
 test <- subset(df_new, sample==FALSE)
 
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+#models lm
+fit <- lm(Target ~., data=train)
+summary(fit)
+#plot(Target ~., data=train)
+#plot(residuals(fit) ~ ., data=train)
+#get accuracy and MSE
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$Target
+MAE <- mae(error)
+R2=summary(fit)$r.squared
+
+#rfe
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(train[,1:16], train[,17], sizes=c(1:16), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
+#get accuracy and MSE
+pred1 <- predict(results, newdata = test)
+error <- pred1 - test$Target
+MAE2 <- mae(error)
+R22=summary(fit)$r.squared
