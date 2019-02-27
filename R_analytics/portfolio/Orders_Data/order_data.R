beab06d801fa92ff196ab8fb81170d087a6f98a2
Edima Udo <edimaudo@gmail.com>
2019-02-27 07:04:50 -0500

    Create order_data.R

---
+++ R_analytics/portfolio/Orders_Data/order_data.R
@@ -0,0 +1,17 @@
+#remove old data
+rm(list=ls())
+#packages
+packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools','readxl')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+df <- read.csv(file.choose()) #online news
+
+glimpse(df)
+
+summary(df)
