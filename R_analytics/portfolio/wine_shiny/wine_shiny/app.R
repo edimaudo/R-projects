2dfb2cc2056f7ee92358de24eaf6d5cfe02e7d4c
Edima Udo <edimaudo@gmail.com>
2019-02-27 22:58:18 -0500

    Create app.R

---
+++ R_analytics/portfolio/wine_shiny/wine_shiny/app.R
@@ -0,0 +1,17 @@
+
+#DASHBOARD DESIGN
+#
+
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
