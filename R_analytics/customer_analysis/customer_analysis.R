1cda8508fed51d814309c58b1247e42df19b3992
Edima Udo <edimaudo@gmail.com>
2019-03-06 06:02:52 -0500

    Update customer_analysis.R

--- R_analytics/customer_analysis/customer_analysis.R
+++ R_analytics/customer_analysis/customer_analysis.R
@@ -1,3 +1,6 @@
+#objective - achieve Lift >4 and Accuracy > 65 for predicting customer answer
+
+#remove old data
 rm(list=ls())
 #packages
 packages <- c('tidyverse','caret','mlbench', 'caTools','readxl','unbalanced','dummies')
@@ -10,22 +13,45 @@ for (package in packages) {
 }
 
 #load data
-df <- read_excel(file.choose())
+df <- read_excel(file.choose()) #data3
 
 glimpse(df)
 
 summary(df)
 
+#backup data
+df.backup <- df
+
 #check for missing data
 
 #check for balance
-
-#get 
-#TNPS_ANSWER_NLP might be the target variable
-#resample data 
-
-#metrics lift and accuracy
-#Metrics: Lift >4 and Accuracy > 65
-
 #ggplot(df,aes(x=as.factor(TNPS_Answer_NLP))) + geom_bar()
+
+
+#cts data
+
+#normalize cts data
+
+#categorical data
+
+#target data - #TNPS_ANSWER_NLP
+
+#combine columns
+
+#remove redudant columns
+
+#split into train and test
+set.seed(123)
+
+#apply algorithms
+
+#check metrics - lift and accuracy
+
+
+
+
+
+
+
+
 
