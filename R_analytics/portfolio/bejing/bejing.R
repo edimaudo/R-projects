2fd60e63e9098a57ff7f0b51a05a5445bf6bbc85
Edima Udo <edimaudo@gmail.com>
2019-03-14 20:41:33 -0400

    Update bejing.R

--- R_analytics/portfolio/bejing/bejing.R
+++ R_analytics/portfolio/bejing/bejing.R
@@ -45,3 +45,41 @@ df[,c(5,6,8,9,10)] <- as.data.frame(lapply(df[,c(5,6,8,9,10)], normalize))
 
 df <- cbind(df,df_cat_new,Target)
 
+#split into train and test
+set.seed(123)
+sample <- sample.split(df,SplitRatio = 0.75)
+train <- subset(df,sample ==TRUE)
+test <- subset(df, sample==FALSE)
+
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+#models lm
+fit <- lm(Target ~., data=train)
+summary(fit)
+#get accuracy and MSE
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$Target
+MAE <- mae(error)
+R2=summary(fit)$r.squared
+
+#rfe
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(train[,1:ncol(train)-1], train[,ncol(train)], sizes=c(1:ncol(train)-1), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
+#get accuracy and MSE
+pred1 <- predict(results, newdata = test)
+error <- pred1 - test$Target
+MAE2 <- mae(error)
+R22=summary(fit)$r.squared
