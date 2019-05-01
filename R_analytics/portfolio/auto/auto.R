470c0e6a0b400ebe11236789175562cad4e35375
Edima Udo <edimaudo@gmail.com>
2019-04-30 20:58:07 -0400

    Update auto.R

--- R_analytics/portfolio/auto/auto.R
+++ R_analytics/portfolio/auto/auto.R
@@ -38,8 +38,61 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 df_cts <- df[,c(3,4,5,6)]
-df_cts <- as.data.frame(lapply(df_cts, normalize)) <- fix
+df_cts <- lapply(df_cts, function(x) as.double(x))
+df_cts <- as.data.frame(lapply(df_cts, normalize))
 
 #combine data
-strength <- as.data.frame(df[,9])
-df_new <- cbind(df_cat_new,df_cts, strength)
+mpg <- as.data.frame(df[1])
+df_new <- cbind(df_cat_new,df_cts, mpg)
+
+#fine tune model
+# # calculate correlation matrix
+correlationMatrix <- cor(df_new[,1:length(df_new)-1])
+# # summarize the correlation matrix
+# print(correlationMatrix)
+# # find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
+# # print indexes of highly correlated attributes
+print(highlyCorrelated)
+
+df_new <- df_new[,-c(highlyCorrelated)]
+
+
+#split data
+set.seed(123)
+sample <- sample.split(df_new,SplitRatio = 0.75)
+train <- subset(df_new,sample ==TRUE)
+test <- subset(df_new, sample==FALSE)
+
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+#models lm
+fit <- lm(mpg ~., data=train)
+summary(fit)
+#get accuracy and MSE
+pred1 <- predict(fit, newdata = test)
+
+error <- pred1 - test$mpg
+MAE <- mae(error)
+R2=summary(fit)$r.squared
+
+#rfe
+# define the control using a random forest selection function
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(train[,1:length(df_new)], train[,length(df_new)], sizes=c(1:length(df_new)), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
+#get accuracy and MSE
+pred1 <- predict(results, newdata = test)
+error <- pred1 - test$mpg
+MAE2 <- mae(error)
+R22=summary(fit)$r.squared
