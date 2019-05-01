097668bb8162164053bc4aae082580cecf6ef150
Edima Udo <edimaudo@gmail.com>
2019-05-01 07:41:47 -0400

    Update energy.R

---
+++ R_analytics/portfolio/energy/energy.R
@@ -0,0 +1,18 @@
+#remove old data
+rm(list=ls())
+#packages
+packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools','readxl')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+df <- read_excel(file.choose()) #data
+
+glimpse(df)
+
+summary(df)
+
