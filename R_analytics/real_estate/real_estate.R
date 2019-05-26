6258291776e9e7f938adb14c9bcf52ec9d890092
Edima Udo <edimaudo@gmail.com>
2019-05-25 20:49:52 -0400

    Update real_estate.R

--- R_analytics/real_estate/real_estate.R
+++ R_analytics/real_estate/real_estate.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 #packages
 packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools','dummies',
-              'factorextra')
+              'factoextra')
 #load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -11,6 +11,14 @@ for (package in packages) {
   }
 }
 
-df <- read.csv(file.choose(), header = FALSE)
+df <- read.csv(file.choose(), header = TRUE)
 
 glimpse(df)
+
+df_update <- df[,c(1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19)]
+
+#normalize data
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+df_cts <- as.data.frame(lapply(df_update, normalize))
