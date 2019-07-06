b301ea01a04c954bb595e058b9cbfa8d2a6b9a41
Edima Udo <edimaudo@gmail.com>
2019-07-06 14:50:29 -0400

    Update app.R

--- shiny/sales/Sales/app.R
+++ shiny/sales/Sales/app.R
@@ -1,11 +1,27 @@
 ## app.R ##
-library(shiny)
-library(shinydashboard)
+#packages
+packages <- c("tidyverse",'shiny','shinydashboard','readxl')
+
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
+file_path <- "Sample - Superstore.xls"
+superstore <- readxl(file.choose())
+
+
 
 ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
-  dashboardBody()
+  dashboardBody(
+    
+  )
 )
 
 server <- function(input, output) { }
