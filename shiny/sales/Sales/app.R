04f2b10d32a5f2e5a023ff4ba0e1a22f31b1a588
Edima Udo <edimaudo@gmail.com>
2019-07-07 14:39:06 -0400

    Update app.R

--- shiny/sales/Sales/app.R
+++ shiny/sales/Sales/app.R
@@ -1,3 +1,5 @@
+#clear data
+rm(list = ls())
 ## app.R ##
 #packages
 packages <- c("tidyverse",'shiny','shinydashboard','readxl')
@@ -13,17 +15,47 @@ for (package in packages) {
 #load data
 file_path <- "Sample - Superstore.xls"
 superstore <- read_excel(file_path)
+newdata <- superstore[order(superstore$State),]
 
+state <- unique(newdata$State)
 
-
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody(
+# Use a fluid Bootstrap layout
+ui <- fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Sales Information by State"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("nameInfo", "State:", 
+                  choices=state)
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("salesCategory")#,
+      #plotOutput("heightWeight"),
+      #plotOutput('gameMedalCount'), 
+      #plotOutput("sportMedalCount")
+    )
     
   )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) { 
+  
+  output$salesCategory <- renderPlot({
+    graphdata <- newdata %>%
+      filter(State %in% input$nameInfo)
+    
+    ggplot(data=graphdata, aes(x=Category, y=Sales)) + 
+      ggtitle("Sales by Category") + theme_bw()
+
+  })
+  
+  }
 
 shinyApp(ui, server)
