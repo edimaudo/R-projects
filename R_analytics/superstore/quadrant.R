e251b091eb0bbecbf10bf5a400b4135dbd0e5677
Edima Udo <edimaudo@gmail.com>
2019-07-13 20:35:48 -0400

    Update quadrant.R

--- R_analytics/superstore/quadrant.R
+++ R_analytics/superstore/quadrant.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 
-packages <- c('ggplot2','readxl','')
+packages <- c('ggplot2','readxl','tidyverse')
 
 #load packages
 for (package in packages) {
@@ -12,4 +12,17 @@ for (package in packages) {
 
 #load data
 #file_path <- "Sample - Superstore.xls"
-df <- read_excel(file.choose())
+df <- read_excel(file.choose(),sheet = "Orders",col_names = TRUE)
+
+df$ProfitRatio <- df$Profit/df$Sales
+
+
+
+p<-ggplot(df, aes(x=ProfitRatio, y=Discount)) +
+  geom_point() +
+  lims(x=c(1,10),y=c(1,10)) +
+  theme_minimal() +
+  coord_fixed() +  
+  geom_vline(xintercept = 5) + geom_hline(yintercept = 5) 
+
+p
