315b5a9e659f4f660e458247a52ba7ddc4d80cf3
Edima Udo <edimaudo@gmail.com>
2019-07-20 19:30:23 -0400

    Update rent_regression.R

--- R_analytics/rent_prediction/rent_regression.R
+++ R_analytics/rent_prediction/rent_regression.R
@@ -1,1 +1,16 @@
 #rent prediction
+rm(list = ls())
+
+#remove old data
+rm(list=ls())
+#packages
+packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 'caTools','dummies')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
