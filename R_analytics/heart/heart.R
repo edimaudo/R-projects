9ec91e8eeb0a43c55d43883be8990b5e1cbb7af9
Edima Udo <edimaudo@gmail.com>
2019-08-29 22:37:36 -0400

    Update heart.R
    updating heart code

--- R_analytics/heart/heart.R
+++ R_analytics/heart/heart.R
@@ -1,8 +1,11 @@
 #objective
+# ------------------------
 #predict the presence or absence of heart disease given various data 
 #about a patient, including resting blood pressure, maximum heart rate, and EKG readings, 
 #as well as other information like age and sex.
-#data description
+# ------------------------
+
+# ------------------------
 # Attribute Information:
 # ------------------------
 # -- 1. age       
@@ -45,14 +48,16 @@
 rm(list=ls())
 
 #load libraries
-for (package in c('ggplot2','corrplot','tidyverse','caret','mlbench')) {
+packages <- c('ggplot2','corrplot','tidyverse','caret','mlbench','mice', 'caTools','dummies')
+
+for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package)
     library(package, character.only=T)
   }
 }
 
-#read file
+#load data
 df <- read.delim(file.choose(),header=FALSE,sep="")
 
 #update column names
@@ -64,76 +69,3 @@ names(df) <- c('age','sex','chest_pain_type','resting_blood_pressure',"serum_cho
 #check for imbalanced data
 ggplot(data=df, aes(x=factor(disease))) +
   geom_bar() + theme_classic() + xlab("Number of heart diseases")  
-
-df.backup <- df
-
-#df <- df.backup
-
-df <- df %>% 
-  select('age', 
-         'sex', 
-         'max_heart', 
-         'resting_blood_pressure', 'disease')
-
-
-df$disease <- recode_factor(df$disease, "1" = "0", "2" = "1")
-
-#remove empty data
-df <- na.omit(df)
-
-library(car)
-scatterplotMatrix(df[,1:4])
-
-#normalize data
-normalize <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x)))
-}
-
-df$age <- normalize((df$age))
-df$max_heart <- normalize((df$max_heart))
-df$resting_blood_pressure <- normalize((df$resting_blood_pressure))
-
-#feature selection
-#Remove Redundant Feature remove when absolute correlation >= 0.75
-set.seed(7)
-# # calculate correlation matrix
-correlationMatrix <- cor(df[,1:4])
-# summarize the correlation matrix
-print(correlationMatrix)
-# find attributes that are highly corrected (ideally >0.75)
-highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
-# print indexes of highly correlated attributes
-print(highlyCorrelated)
-#no columns to remove
-
-#machine learing
-#split data into train and test
-library(caTools)
-set.seed(123)
-
-sample <- sample.split(df,SplitRatio = 0.75)
-train <- subset(df,sample ==TRUE)
-test <- subset(df, sample==FALSE)
-
-#models
-model_gbm<-train(train[,1:4],train[,5],method='gbm')
-model_rf<-train(train[,1:4],train[,5],method='rf')
-model_nnet<-train(train[,1:4],train[,5],method='nnet')
-
-#Predictions
-predictions<-predict.train(object=model_gbm,test,type="raw")
-#table(predictions)
-confusionMatrix(predictions,test[,5])
-
-predictions_rf<-predict.train(object=model_rf,test,type="raw")
-confusionMatrix(predictions_rf,test[,5])
-
-predictions_nnet<-predict.train(object=model_nnet,test,type="raw")
-confusionMatrix(predictions_nnet,test[,5])
-
-
-
-
-
-
-
