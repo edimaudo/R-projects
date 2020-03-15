36dbb87f7b9630e1144731e405bab8a2ff0443c8
Edima <edimaudo@gmail.com>
2020-03-15 18:12:53 -0400

    Create server.R

---
+++ shiny/bookRecommender/old/server.R
@@ -0,0 +1,30 @@
+#
+# This is a data explorer and Book recommender system
+# 
+
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', 
+              'caTools','dummies','ggfortify','shiny')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
