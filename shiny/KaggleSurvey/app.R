bed3978bffcbc2a52f22e685973703ceb04d24d0
Edima <edimaudo@gmail.com>
2020-03-23 17:35:18 -0400

    Update app.R

--- shiny/KaggleSurvey/app.R
+++ shiny/KaggleSurvey/app.R
@@ -1,8 +1,3 @@
-#Monday
-#add side bar panel for country comparison tab
-#add two drop downs with county information sorted in ascending order for country comparison tab
-#add a submit button
-
 #Tuesday
 #clean up code for different columns that had fix code
 #change size of the graphs of all the visualizations
@@ -55,7 +50,7 @@ country <- responses %>%
   arrange(Q3) %>%
   select(Q3)
 
-country <- unique(country)
+country <- as.vector(unique(country$Q3))
   
 
 ui <- fluidPage(
@@ -107,30 +102,61 @@ ui <- fluidPage(
                         plotOutput('enterpriseusage'),
                         hr()),
                tabPanel("Country Comparison",
-                        sidebarLayout(
                           sidebarPanel(
-                            selectInput("countryInput", "Country",choices=country)
-                          )),
+                            helpText("Select a country from the two drop down lists"),
+                            selectInput("countryInput1", "Country 1",choices=country),
+                            selectInput("countryInput2", "Country 2",choices=country),
+                            br(),
+                            submitButton("Submit")
+                          ),
                         mainPanel(
                           plotOutput("coolplot"),
                           br(), br()
-                          
                         ),
                         hr())
              ))
 )
 
-# Define server logic required to draw a histogram
+#gender
+genderinfo <- responses %>%
+  select(Q2) %>%
+  group_by(Q2) %>%
+  summarize(freq = n()) %>%
+  arrange(desc(freq)) 
+
+#age
+ageinfo <- responses %>%
+  select(Q1) %>%
+  group_by(Q1) %>%
+  summarise(freq = n())
+
+#education degrees
+educationinfo <- responses %>%
+  select(Q4) %>%
+  group_by(Q4) %>%
+  summarise(freq = n())
+educationinfo <- na.omit(educationinfo)
+
+#time learning to code
+timeleanringinfo <- responses %>%
+  select(Q15) %>%
+  group_by(Q15) %>%
+  summarise(freq = n())
+timeleanringinfo <- na.omit(timeleanringinfo)
+
+#time machine learning
+timeleanringmachineinfo <- responses %>%
+  select(Q23) %>%
+  group_by(Q23) %>%
+  summarise(freq = n())
+timeleanringmachineinfo <- na.omit(timeleanringmachineinfo)
+
+
+# Define server logic
 server <- function(input, output) {
   
   #gender
   output$genderplot <- renderPlot({
-    genderinfo <- responses %>%
-      select(Q2) %>%
-      group_by(Q2) %>%
-      summarize(freq = n()) %>%
-      arrange(desc(freq)) 
-    
     ggplot(data=genderinfo, aes(x=reorder(Q2,-freq), y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + labs(x = "Gender", y = "Count") +
       theme(axis.text.x = element_text(angle = 0, hjust = 1),
@@ -139,11 +165,6 @@ server <- function(input, output) {
   
   #age
   output$ageplot <- renderPlot({
-    ageinfo <- responses %>%
-      select(Q1) %>%
-      group_by(Q1) %>%
-      summarise(freq = n())
-    
     ggplot(data=ageinfo, aes(x=Q1, y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + labs(x = "Age", y = "Count") +
       theme(axis.text.x = element_text(angle = 0, hjust = 1),legend.position="none")
@@ -166,13 +187,6 @@ server <- function(input, output) {
   
   #Education degrees
   output$educationplot <- renderPlot({
-    
-    educationinfo <- responses %>%
-      select(Q4) %>%
-      group_by(Q4) %>%
-      summarise(freq = n())
-    
-    educationinfo <- na.omit(educationinfo)
     ggplot(data=educationinfo, aes(x=reorder(Q4,-freq), y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + labs(x = "Degree", y = "Count") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1),legend.position="none") 
@@ -186,12 +200,6 @@ server <- function(input, output) {
   
   #time learning to code
   output$timelearningcodeplot <- renderPlot({
-    timeleanringinfo <- responses %>%
-      select(Q15) %>%
-      group_by(Q15) %>%
-      summarise(freq = n())
-    
-    timeleanringinfo <- na.omit(timeleanringinfo)
     ggplot(data=timeleanringinfo, aes(x=reorder(Q15,-freq), y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + labs(x = "Time learning to code", 
                                                                           y = "Count") +
@@ -200,12 +208,6 @@ server <- function(input, output) {
   
   #time machine learning
   output$timemachinelearningplot <- renderPlot({
-    timeleanringmachineinfo <- responses %>%
-      select(Q23) %>%
-      group_by(Q23) %>%
-      summarise(freq = n())
-    
-    timeleanringmachineinfo <- na.omit(timeleanringmachineinfo)
     ggplot(data=timeleanringmachineinfo, aes(x=reorder(Q23,-freq), y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + labs(x = "Time spent machine learning", 
                                                                           y = "Count") +
@@ -267,8 +269,8 @@ server <- function(input, output) {
       select(Q8) %>%
       group_by(Q8) %>%
       summarise(freq = n())
-    
     enterpriseinfo <- na.omit(enterpriseinfo)
+    
     ggplot(data=enterpriseinfo, aes(x=Q8, y=freq)) +
       geom_bar(stat="identity",fill="steelblue") + theme_classic() + 
       labs(x = "Enterprise machine learning adoption", 
