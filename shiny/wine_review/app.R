7f095bc8186e904e302799e21a0850cb595ea749
Edima <edimaudo@gmail.com>
2020-03-26 19:56:42 -0400

    Update app.R

--- shiny/wine_review/app.R
+++ shiny/wine_review/app.R
@@ -1,48 +1,29 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Shiny web app that insights into prices, ratings, 
+# descriptions, and geographic distribution of the world's most esteemed wines
 
-library(shiny)
+
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','dummies','ggfortify','shiny',
+              'shinydashboard','countrycode','highcharter',"gridExtra")
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
 }
 
 # Run the application 
