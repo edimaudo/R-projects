d55b88b3fd8503304399cf264ca53dc74fb3a291
Edima <edimaudo@gmail.com>
2020-04-13 12:30:50 -0400

    Update app.R

---
+++ shiny/wine_review/app.R
@@ -0,0 +1,36 @@
+#
+# Shiny web app that insights into prices, ratings, 
+# descriptions, and geographic distribution of the world's most esteemed wines
+#
+
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','shiny',
+              'shinydashboard','countrycode','highcharter',"gridExtra")
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
+wine_data <- load("wine_dfR.RData")
+
+variety <- as.vector(unique(wine_df$variety))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
