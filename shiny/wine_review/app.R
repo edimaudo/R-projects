ba2cbefb005c9f353e05792ebd4c50d701ea3eb5
Edima <edimaudo@gmail.com>
2020-04-13 12:35:51 -0400

    Update app.R

--- shiny/wine_review/app.R
+++ shiny/wine_review/app.R
@@ -1,3 +1,33 @@
+# review data - 
+# review what other people did - 
+# design what visualization is needed 
+# design readme - 
+# design about section for the app - 
+# Review libraries to use - 
+# Design the layout of the app - 
+# build initial layout - 
+#build introduction - 
+#create reminaing tabs layout - 
+#build background - add it with centered layout - 
+#use boxes and summary information layout for the background - 
+#wine selector varietal + price + rating + top 10 outputs table layout - 
+#have rating drop down + visualization for prcies, country & variety layout - 
+#have price drop down + visualization for rating, country & variety layout - 
+#have variety checkboxes + visualization for rating, country, prices layout - 
+#debug issues for layouts + test - sat - 
+#wine selector varietal + price + rating + top 10 outputs table layout 
+
+#wine selector varietal + price + rating + top 10 outputs table code 
+
+#use boxes and summary information layout + code + test 
+
+#have rating drop down + visualization for prcies, country & variety code + test
+#have price drop down + visualization for rating, country & variety code + test
+#have variety dropdown + visualization for rating, country, prices code + test
+
+#wine recommendation using user layout + recommendation code + test
+
+
 #
 # Shiny web app that insights into prices, ratings, 
 # descriptions, and geographic distribution of the world's most esteemed wines
