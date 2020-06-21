da7d229fa6d5719c617af49169c7fecb0e320f52
Edima <edimaudo@gmail.com>
2020-06-21 19:21:48 -0400

    Update app.R

--- shiny/contract/app.R
+++ shiny/contract/app.R
@@ -8,3 +8,42 @@ for (package in packages) {
     library(package, character.only=T)
   }
 }
+
+#load data
+df <- read_csv("Recent_Contract_Awards.csv")
+
+agency <- c(sort(unique(df$AgencyName)))
+category <- c(sort(unique(df$AgencyName)))
+selectMethod <- c(sort(unique(df$SelectionMethodDescription)))
+
+
+# Define UI for application
+ui <- dashboardPage(
+  dashboardHeader(title = "Contract Information"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Introduction", tabName = "Introduction", icon = icon("dashboard")),
+      menuItem("Agency", tabName = "agency", icon = icon("th")),
+      menuItem("Category", tabName = "category", icon = icon("th")),
+      menuItem("selection Method", tabName = "selectionMethod", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "Introduction",includeMarkdown("readme.md"),hr()),
+      tabItem(tabName = "agency",),
+      tabItem(tabName = "category",),
+      tabItem(tabName = "selectionMethod",)
+    )
+  )
+)
+
+
+
+server <- function(input, output) {
+
+  
+  }
+
+# Run the application 
+shinyApp(ui = ui, server = server)
