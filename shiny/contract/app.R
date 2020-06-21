0f759662ae1e510abc9b93f5f570c15e6e890e79
Edima <edimaudo@gmail.com>
2020-06-21 17:33:14 -0400

    Update app.R

--- shiny/contract/app.R
+++ shiny/contract/app.R
@@ -1,1 +1,10 @@
 
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','shiny','shinydashboard')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
