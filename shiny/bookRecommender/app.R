66222405c0a4599df92c212658d1caa577e44aa4
Edima <edimaudo@gmail.com>
2020-06-23 09:54:44 -0400

    Update app.R

--- shiny/bookRecommender/app.R
+++ shiny/bookRecommender/app.R
@@ -1,13 +1,9 @@
 #
-# This is a data explorer and Book recommender system
+#Book recommender system
 # 
 
-
-#remove old data
-rm(list=ls())
 #packages 
-packages <- c('ggplot2', 'corrplot','tidyverse','caret','mlbench','mice', "DT",
-              'caTools','dummies','ggfortify','shiny', 'recommenderlab','proxy','reshape2')
+packages <- c('shiny', 'recommenderlab','proxy','reshape2')
 #load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -15,45 +11,15 @@ for (package in packages) {
     library(package, character.only=T)
   }
 }
+source("helpercode.R")
 
-#load data
-users <- read_csv("BX-Users_clean.csv")
 books <- read_csv("BX-Books_clean.csv")
 ratings <- read_csv("BX-Book-Ratings_clean.csv")
-
-bookTitles <- unique(books$bookTitle)
-
-book_recommendation <- function(input,input2,input3) {
-  row_num <- which(books[,2] == input)
-  row_num2 <- which(books[,2] == input2)
-  row_num3 <- which(books[,2] == input3)
-  userSelect <- matrix(NA,8552)
-  userSelect[row_num] <- 5 #hard code first selection to rating 5
-  userSelect[row_num2] <- 4 #hard code second selection to rating 4
-  userSelect[row_num3] <- 3 #hard code third selection to rating 3
-  userSelect <- t(userSelect)
-  
-  ratingmat <- dcast(ratings, userID~ISBN, value.var = "bookRating", na.rm=FALSE)
-  ratingmat <- ratingmat[,-1]
-  colnames(userSelect) <- colnames(ratingmat)
-  ratingmat2 <- rbind(userSelect,ratingmat)
-  ratingmat2 <- as.matrix(ratingmat2)
-  
-  #Convert rating matrix into a sparse matrix
-  ratingmat2 <- as(ratingmat2, "realRatingMatrix")
-  
-  #Create Recommender Model. "UBCF" stands for user-based collaborative filtering
-  recommender_model <- Recommender(ratingmat2, method = "UBCF",
-                                   param=list(method="Cosine",nn=30))
-  recom <- predict(recommender_model, ratingmat2[1], n=10)
-  recom_list <- as(recom, "list")
-  recom_result <- data.frame(matrix(NA,10))
-  for (i in c(1:10)){
-    recom_result[i,1] <- bookTitles[as.integer(recom_list[[1]][i]),2]
-  }
-  colnames(recom_result) <- "User-Based Collaborative Filtering Recommended Titles"
-  return(recom_result)
-}
+bookIds <- length(unique(books$ISBN)) #271360
+ratingbookIds <- length(unique(ratings$ISBN)) #149836
+books2 <- books[-which((bookIds %in% ratingbookIds) == FALSE),]
+book2 <- book2 %>%
+  top_n(5000,ISBN)
 
 
 ui <- fluidPage(
@@ -64,23 +30,24 @@ ui <- fluidPage(
       tabPanel("Book Recommender", fluid = TRUE,
                  mainPanel(
                    fluidRow(
+                     h3("Choose Three books You Like"),
                      column(5,
-                            selectInput("select", 
-                                        label = h3("Choose Three books You Like"),
-                                        choices = as.character(bookTitles[1:1000])),
-                            selectInput("select2", label = NA,
-                                        choices = as.character(bookTitles[1:1000])),
-                            selectInput("select3", label = NA,
-                                        choices = as.character(bookTitles[1:1000])),
+                            selectInput("select", label = "Book 1",
+                                        choices = as.character(book2$bookTitle[1:1000])),
+                            selectInput("select2", label = "Book 2",
+                                        choices = as.character(book2$bookTitle[1001:2000])),
+                            selectInput("select3", label = "Book 3",
+                                        choices = as.character(book2$bookTitle[2001:3000])),
                             submitButton("Submit")
                       ),
-                  ),
-                     fluidRow(
+                  column(7,
                             h3("You Might Like These Too!"),
-                            DT::dataTableOutput("table"))
+                            DT::dataTableOutput("table")
+                         )
                      )
+                  )
       )
-    )
+  )
 )
   
 server = function(input, output) {
