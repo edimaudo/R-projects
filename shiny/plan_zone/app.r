211b6b40952c7e2224416c143390c9aff0388230
Edima <edimaudo@gmail.com>
2020-07-09 22:05:22 -0400

    Update app.r

--- shiny/plan_zone/app.r
+++ shiny/plan_zone/app.r
@@ -1,1 +1,18 @@
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','ggfortify','shiny','readxl','DT')
+#load packages
+for (package in packages) {
+    if (!require(package, character.only=T, quietly=T)) {
+        install.packages(package)
+        library(package, character.only=T)
+    }
+}
+
+#load data
+df <- read_excel("zone_data.xlsx")
+
+#pytpe
+ptype <- c(sort(unique(df$p_type)))
+region <- c(sort(unique(df$region)))
+
 
