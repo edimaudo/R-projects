9188bd001c816a8e9b36dc4e484d738367db0fa3
Edima <edimaudo@gmail.com>
2020-07-09 22:34:57 -0400

    Update app.r

--- shiny/plan_zone/app.r
+++ shiny/plan_zone/app.r
@@ -15,4 +15,23 @@ df <- read_excel("zone_data.xlsx")
 ptype <- c(sort(unique(df$p_type)))
 region <- c(sort(unique(df$region)))
 
+ui <- fluidPage(
+    h1("Unit analysis",style="text-align: center;"), 
+    sidebarPanel(
+        helpText("Select a Price"),
+        selectInput("priceInput", label = "Prices",choices = price_ranges)
+    ),
+    mainPanel(
+        fluidRow(
+            h3("Ratings",style="text-align: center;"),
+            plotOutput("ratingPriceplot"),
+        )
+    )
+)
 
+
+# Define server logic 
+server <- function(input, output) {}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
