b4caecc3b348457f5c286e7ca153f0fedd3d5eab
Edima <edimaudo@gmail.com>
2020-07-25 18:57:29 -0400

    Update app.R

--- shiny/oil_demand/app.R
+++ shiny/oil_demand/app.R
@@ -12,6 +12,24 @@ for (package in packages) {
 #load data
 df <- read_csv("applemobilitytrends-2020-07-23.csv")
 
+df <- read_csv("applemobilitytrends-2020-07-23.csv")
+
+df <- select (df,-c('geo_type','alternative_name','country','sub-region','country',
+                    '2020-05-11','2020-05-12'))
+
+
+mobility_df <- df %>%
+    pivot_longer(-c(region,transportation_type), 
+                 names_to = "date_info", values_to = "sum")
+
 region_info <- sort(unique(df$region))
 transportation_type_info <- sort(unique(df$transportation_type))
+day_info <- sort(unique(mobility_df$date_info))
 
+
+#changes in requests for directions by transportation type for all 
+#available countries/regions, sub-regions, and cities.
+
+#dashboard info
+#Mobility Trends
+#Change in routing requests since January 13, 2020
