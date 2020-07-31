d2c2c2e237f86c3b25627b63c4b31b5f0b3258f2
Edima <edimaudo@gmail.com>
2020-07-30 20:25:12 -0400

    Update customer_rank.R

--- R_analytics/customer_rank/customer_rank.R
+++ R_analytics/customer_rank/customer_rank.R
@@ -13,3 +13,20 @@ for (package in packages) {
 
 df <- read_excel("Customer data.xlsx")
 
+df_agg <- df %>%
+  group_by(`Customer name`) %>%
+  summarise(total = sum(`Sales (AUD)`)) %>%
+  select(`Customer name`,total) %>%
+  arrange(desc(total))
+
+df_agg <- na.omit(df_agg)
+
+
+ggplot(df_agg, aes(x = reorder(`Customer name`, -total),y=total)) + 
+  geom_bar(stat = "identity",width = 0.5, fill="steelblue") + theme_classic() + 
+  labs(x = "Customer Name", y = "Total Sales (AUD)") +
+  theme(legend.text = element_text(size = 15),
+        legend.title = element_text(size = 15),
+        axis.title = element_text(size = 20),
+        axis.text = element_text(size = 15),
+        axis.text.x = element_text(angle = 45, hjust = 1))
