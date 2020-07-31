29a0312481d512b8527aa22b6ad63e84b8bdc685
Edima <edimaudo@gmail.com>
2020-07-30 22:17:25 -0400

    Update aluminum_grade_LP.R

--- R_analytics/LP_model/aluminum_grade_LP.R
+++ R_analytics/LP_model/aluminum_grade_LP.R
@@ -14,7 +14,35 @@ for (package in packages) {
 #objective 
 #Find out the number of days to operate 
 #each mill to meet the contract at the minimum cost
-#United Aluminum Company of Cincinnati produces three grades (high, medium, and low) of aluminum at two mills. 
+#United Aluminum Company of Cincinnati produces three grades (high, medium, and low) of 
+#aluminum at two mills. 
 #Each mill has a different production capacity (in tons per day) for each grade
 
+objective.in=c(6000,7000) #costs for mills
+
+#Constraint Matrix
+const.mat=matrix(c(6,2,2,2,4,10),nrow = 3,byrow = T) #high, medium, low grade
+
+#defining constraints
+const_high=12  
+const_medium=8
+const_low =5
+
+#RHS for constraints
+const.rhs=c(const_high, const_medium, const_low)
+
+#Direction for constraints
+const.dir=c(">=",">=",">=")
+
+
+#Finding the optimum solution
+opt=lp(direction = "min",objective.in,const.mat,const.dir,const.rhs)
+summary(opt)
+
+
+#Objective values
+opt$solution
+
+#Value of objective function at optimal point
+opt$objval
 
