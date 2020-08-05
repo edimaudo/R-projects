86aa53bb255551f8690a544a3c77425046a0460e
Edima <edimaudo@gmail.com>
2020-08-05 12:47:54 -0400

    Create store_forecast.R

---
+++ R_analytics/store_forecast/store_forecast.R
@@ -0,0 +1,25 @@
+
+
+##Objective
+#Create a timeseries forecasting model for daily sales prediction.
+#The model should incorporate multiple seasonalities 
+#and lags for the promotional periods. 
+#There are over 1100 different stores, with 4 categories. 
+#The model(s) should be able to make forecast based on the store and type of store. 
+#validate the model trend, multiple seasonalities, and lags 
+
+
+
+#load data
+
+#combine data
+
+#https://rpubs.com/steffanigomez/forecastingtutorial
+#https://rstudio-pubs-static.s3.amazonaws.com/300439_029f7ba592794d1cb2dac8f09d139f51.html
+#https://github.com/rjshanahan/Time-Series-Forecasting-with-Shiny
+
+#build shiny framework
+
+#build models
+
+#build shiny model app
