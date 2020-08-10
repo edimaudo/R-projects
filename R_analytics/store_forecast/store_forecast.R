3a6587e257aea5e28b5791ee6f8d8e4ff06220fb
Edima <edimaudo@gmail.com>
2020-08-10 11:56:34 -0400

    Update store_forecast.R

--- R_analytics/store_forecast/store_forecast.R
+++ R_analytics/store_forecast/store_forecast.R
@@ -8,18 +8,36 @@
 #The model(s) should be able to make forecast based on the store and type of store* - maybe look at categotity 
 #validate the model trend, multiple seasonalities, and lags 
 
+rm(list = ls())
+#packages 
+packages <- c('ggplot2', 'corrplot','tidyverse','shiny','shinydashboard',
+              'SnowballC','wordcloud','dplyr','tidytext','readxl','scales','forecast','prophet')
+#load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
 
 
 #load data
+store <- read.csv("store.csv")
+train <- read.csv("train.csv")
+
+glimpse(store)
+glimpse(train)
 
 #combine data
+
+store_train <- train %>%
+  inner_join(store, by = "Store")
+
+#build forecasting model
 
 #https://rpubs.com/steffanigomez/forecastingtutorial
 #https://rstudio-pubs-static.s3.amazonaws.com/300439_029f7ba592794d1cb2dac8f09d139f51.html
 #https://github.com/rjshanahan/Time-Series-Forecasting-with-Shiny
 
-#build shiny framework
-
-#build models
 
 #build shiny model app
