cebf3cdbd2130b345fb583a3258eaa2a4ba84f25
Edima <edimaudo@gmail.com>
2020-10-15 20:19:07 -0400

    Update app.R

--- shiny/shinyInvoice/app.R
+++ shiny/shinyInvoice/app.R
@@ -11,3 +11,36 @@ for (package in packages) {
 
 #load data
 df <- read_excel("Sales Detail Report.xlsx")
+invoice <- sort(as.vector(unique(as.integer(df$SaleID))))
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Side Project SaaS Price Generator"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Sales Invoice", tabName = "salesInvoice", icon = icon("th"))
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "priceGenerator",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput("yearInput", "Year:", choices = yearData),
+                            br(),
+                            submitButton("Submit")
+                        ),
+                        mainPanel(fluidRow(
+                            h2("Text mining insights", style = "text-align: center;"),
+                            plotOutput("generateProgramSentiment")
+                        ))
+                    )
+            )
+        )
+    )
+)
+
+server <- function(input, output, session) {
+    
+}
+
+shinyApp(ui, server)
