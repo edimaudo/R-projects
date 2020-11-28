449fd0c26967cb36f4033fec3ab0fac9d875a6a5
Edima <edimaudo@gmail.com>
2020-11-27 19:39:51 -0500

    Update pakistan_airline.R

--- R_analytics/pakistan_airlines/pakistan_airline.R
+++ R_analytics/pakistan_airlines/pakistan_airline.R
@@ -21,3 +21,12 @@ review_df <- data.frame(matrix(ncol = 4, nrow = 0))
 review_col <- c("title","description","date_information","rating",)
 colnames(review_df) <- review_col
 
+url <- "https://www.tripadvisor.com/Airline_Review-d8729129-Reviews-or5-Pakistan-International-Airlines.html"
+
+#function to manage connection closing and reduce 403 issues
+CatchupPause <- function(Secs){
+  Sys.sleep(Secs) #pause to let connection work
+  closeAllConnections()
+  gc()
+}
+
