ca0cae83e61cbd050b4c3eb80f22b202cd496aed
Edima <edimaudo@gmail.com>
2020-12-03 12:44:48 -0500

    Add files via upload

---
+++ R_analytics/amazon_reviews/amazon_review.R
@@ -0,0 +1,12 @@
+# =======================================================
+# clear environment
+# =======================================================
+rm(list=ls())
+
+# =======================================================
+# load packages
+# =======================================================
+packages <- c('ggplot2', 'corrplot','tidyverse','dplyr','tidyr',
+              'caret','mlbench','mice','scales','proxy','reshape2',
+              'caTools','dummies','scales','catboost', 'Matrix',
+              'stringr','reshape2','purrr','lubridate')
