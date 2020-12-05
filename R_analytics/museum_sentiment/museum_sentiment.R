46b2a3a406af0fdfd046429af0441fe43e393d71
Edima <edimaudo@gmail.com>
2020-12-04 21:44:40 -0500

    Add files via upload

---
+++ R_analytics/museum_sentiment/museum_sentiment.R
@@ -0,0 +1,19 @@
+# =======================================================
+# packages
+# =======================================================
+rm(list=ls())
+packages <- c('ggplot2', 'corrplot','tidyverse','dplyr','tidyr',
+              'caret','mlbench','scales','proxy','reshape2',
+              'caTools','doParallel','scales','catboost', 'Matrix',
+              'stringr','reshape2','purrr','lubridate','tidytext','gridExtra',
+              'wordcloud2','stringr','tm','SnowballC')
+# load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+#load data
+df <- read.csv(file.choose(),sep = ",")
