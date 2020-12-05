8113b325dfd905f70c34b092d571e5fe53992abd
Edima <edimaudo@gmail.com>
2020-12-04 20:54:06 -0500

    Update amazon_review.R

--- R_analytics/amazon_reviews/amazon_review.R
+++ R_analytics/amazon_reviews/amazon_review.R
@@ -6,7 +6,7 @@ packages <- c('ggplot2', 'corrplot','tidyverse','dplyr','tidyr',
               'caret','mlbench','scales','proxy','reshape2',
               'caTools','doParallel','scales','catboost', 'Matrix',
               'stringr','reshape2','purrr','lubridate','tidytext','gridExtra',
-              'wordcloud2','stringr')
+              'wordcloud2','stringr','tm','SnowballC')
 # load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -16,6 +16,25 @@ for (package in packages) {
 }
 
 #load data
-df <- read.csv
+df <- read.csv(file.choose(),sep = ",")
 
-#
+glimpse(df)
+
+colnames(df)
+
+unique(df$Category)
+unique(df$Procuct)
+unique(df$Country)
+unique(df$Date)
+unique(df$Year)
+unique(df$Verified)
+
+#select only needed columns
+df <- df %>%
+  select(Procuct,Country,Month,Year, Verified, Helpful,Title, Body, Rating) %>%
+  na.omit()
+
+#check for missing values
+missing_data <- apply(df, 2, function(x) any(is.na(x)))
+print(missing_data)
+
