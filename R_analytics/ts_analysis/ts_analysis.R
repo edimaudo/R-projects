23c85168577dff613f9b85485d620f3be290a98b
Edima <edimaudo@gmail.com>
2020-12-10 21:09:20 -0500

    Update ts_analysis.R

--- R_analytics/ts_analysis/ts_analysis.R
+++ R_analytics/ts_analysis/ts_analysis.R
@@ -3,10 +3,9 @@
 # =======================================================
 rm(list=ls())
 packages <- c('ggplot2', 'corrplot','tidyverse','dplyr','tidyr',
-              'caret','mlbench','scales','proxy','reshape2',
-              'caTools','doParallel','scales','catboost', 'Matrix',
-              'stringr','reshape2','purrr','lubridate','tidytext','gridExtra',
-              'wordcloud2','stringr','tm','SnowballC')
+              'caret','mlbench','caTools','scales','readxl',
+              'doParallel','scales','catboost', 'Matrix','lubridate',
+              'xts','TTR','forecast')
 # load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -14,3 +13,14 @@ for (package in packages) {
     library(package, character.only=T)
   }
 }
+
+df <- read_excel(file.choose())
+
+
+#This converts the dataframe into an xts object
+df.xts <- xts(x = df$Percentage, order.by = df$Months) 
+
+df.end <- floor(0.6*length(df.xts)) #select the first 60% of the data
+df.train <- df.xts[1:df.end,] #assign the first 60% of the data to the train set
+df.test <- df.xts[(df.end+1):length(df.xts),] #assign the most recent 40% to the test set
+
