2f3910c465fda1dc051bbf48b1ad4ebc41177ff4
Edima <edimaudo@gmail.com>
2020-12-13 18:50:40 -0500

    Update app.R

--- shiny/residential_rebate/app.R
+++ shiny/residential_rebate/app.R
@@ -17,6 +17,21 @@ df[is.na(df)] <- 0 #replace na with 0
 area <- c(sort(unique(df$Area)))
 fiscal_year <- c(sort(unique(df$`Fiscal Year`)))
 rebate_type <- c(sort(unique(df$Type)))
+colnms=c("July","August","September","October","November",
+         "December","January","February","March","April","May","June")
+
+df$month_total <- rowSums(df[,colnms])
+
+#todo
+# Introduction
+# - create readme & add it to introduction
+# Summary
+# - build summary table with fiscal year as the dropdown for Summary section
+#  - It should have a dataable with the different columns as per the excel sheet and the calculations
+# Trend
+#- line chart trend for 2012 to 2020 using program type filters
+#- District Devices/		Gallons Saved		AF Saved		AF Saved by year
+#- funding by fiscal year
 
 #app
 ui <- dashboardPage(
@@ -41,9 +56,7 @@ ui <- dashboardPage(
 #server info
 server <- function(input, output) {
     
-    #line chart trend for 2012 to 2020 using program type filters
-    #District Devices/		Gallons Saved		AF Saved		AF Saved by year
-    #funding by fiscal year
+
 }
 
 shinyApp(ui, server)
