b2f8a568e372c9c3c026516b8c72a4d6caf3fee9
Edima <edimaudo@gmail.com>
2020-12-26 13:55:26 -0500

    Update churn_analysis.R

--- R_analytics/customer_churn_analysis/churn_analysis.R
+++ R_analytics/customer_churn_analysis/churn_analysis.R
@@ -32,3 +32,32 @@ print(missing_data) # Total Charges
 #remove NAs
 df <- na.omit(df)
 
+#convert from string to numeric
+df <- as.data.frame(df, stringsAsFactors=F)
+
+#Label Encoder
+labelEncoder <-function(x){
+  as.numeric(factor(x))-1
+}
+#normalize data
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+df_customerID <- df[1]
+df[1] <- NULL
+
+#split data into different categories
+df_cts <- df %>%
+  select(tenure, TotalCharges)
+
+df_cat <- df %>%
+  select(-c(MonthlyCharges,TotalCharges, tenure, Churn))
+
+df_churn <- df %>%
+  select(Churn)
+
+#recode data
+
+#combine data
+df_new <- cbind(df_cts,df_cat,df_churn)
