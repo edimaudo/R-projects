9bf2fb80e5cc91e4abc91ef770df91e06d5e4ea8
Edima <edimaudo@gmail.com>
2021-01-23 17:37:55 -0500

    Add files via upload

---
+++ R_analytics/covid/us_covid.R
@@ -0,0 +1,23 @@
+#===================
+## Load Libraries
+#===================
+rm(list = ls()) #clear environment
+
+# libraries
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
+              ,'FactoMineR','factoextra','scales','dplyr','mlbench','caTools',
+              'gridExtra','doParallel')
+# load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+
+#load data
+df <- read.csv("US_COVID.csv")
+
+
+
