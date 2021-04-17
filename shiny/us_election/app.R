4d569b1c5c77c6c74aaa6be4d4b120ffc5bd68fc
Edima <edimaudo@gmail.com>
2021-04-16 20:57:46 -0400

    Update app.R

--- shiny/us_election/app.R
+++ shiny/us_election/app.R
@@ -20,8 +20,54 @@ presidents <- sort(as.vector(unique(df$President)))
 presidential_party <- sort(as.vector(unique(df$Presidential.Party)))
 market <- sort(as.vector(unique(df$Market)))
 
+# Define UI for application
 ui <- dashboardPage(
-    
+    dashboardHeader(title = "Kickstarter Analysis"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("About", tabName = "about", icon = icon("dashboard")),
+            menuItem("Presidential party", tabName = "party", icon = icon("list-alt")),
+            menuItem("Presidents", tabName = "president", icon = icon("th")),
+            
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "about",includeMarkdown("readme.md"),hr()),
+            tabItem(tabName = "party",
+                    mainPanel(
+                        h2("Summary",style="text-align: center;"),
+                        #fluidRow(valueBoxOutput("countryOutput"),
+                        #         valueBoxOutput("ciytOutput")),
+                        #fluidRow(
+                        #    valueBoxOutput("categoryOutput"),
+                        #    valueBoxOutput("subCategoryOutput")
+                        #),
+                        #fluidRow(valueBoxOutput("amountRaisedOutput"),
+                        #         valueBoxOutput("percentofSuccessfulProjectsOutput")
+                        #)
+                    )
+            ),
+            tabItem(tabName = "president",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput("cityInput", "City", choices = cities),
+                            selectInput("categoryInput", "Category", choices = categories)
+                        ),
+                        mainPanel(
+                            h2("City Analysis",style="text-align: center;"), 
+                            fluidRow(
+                                h3("Amount pledged",style="text-align: center;"),
+                                plotOutput("pledgeYearOutput"),
+                                h3("# of pledges",style="text-align: center;"),
+                                plotOutput("pledgenumYearOutput")
+                                #plotOutput(""),
+                            )
+                        )
+                    )
+            ) # prediction model
+        )
+    )
 )
 
 server <- function(input, output,session) {
