1bee8f73c177aa2b01185b996fb23aa75c1eaeac
Edima <edimaudo@gmail.com>
2021-06-06 12:35:23 -0400

    Update app.R

--- shiny/sales_forecast/app.R
+++ shiny/sales_forecast/app.R
@@ -23,6 +23,68 @@ df$revenue <- df$average_price*df$total_volume #revenue
 
 # Drop downs
 type_info <- sort(as.vector(unique(df$type))) #type 
+type_info <- c(type_info,"All")
 region_info <- sort(as.vector(unique(df$region))) #region
+region_info <- c(region_info,"All")
+forecast_info <- c(12,24) #forecast range
+
+# Define UI for application
+ui <- dashboardPage(
+  dashboardHeader(title = "Kickstarter Analysis"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("dashboard")),
+      menuItem("Summary", tabName = "summary", icon = icon("list-alt")),
+      menuItem("City Analysis", tabName = "city", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "about",includeMarkdown("readme.md"),hr()),
+      tabItem(tabName = "summary",
+              mainPanel(
+                h2("Summary",style="text-align: center;"),
+                fluidRow(valueBoxOutput("countryOutput"),
+                         valueBoxOutput("ciytOutput")),
+                fluidRow(
+                  valueBoxOutput("categoryOutput"),
+                  valueBoxOutput("subCategoryOutput")
+                ),
+                fluidRow(valueBoxOutput("amountRaisedOutput"),
+                         valueBoxOutput("percentofSuccessfulProjectsOutput")
+                )
+              )
+      ),
+      tabItem(tabName = "city",
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("cityInput", "City", choices = cities),
+                  selectInput("categoryInput", "Category", choices = categories)
+                ),
+                mainPanel(
+                  h2("City Analysis",style="text-align: center;"), 
+                  fluidRow(
+                    h3("Amount pledged",style="text-align: center;"),
+                    plotOutput("pledgeYearOutput"),
+                    h3("# of pledges",style="text-align: center;"),
+                    plotOutput("pledgenumYearOutput")
+                    #plotOutput(""),
+                  )
+                )
+              )
+      )
+    )
+  )
+)
+
+
+# Define server logic 
+server <- function(input, output,session) {
+
+  
+  }
+
+shinyApp(ui, server)
+
 
 
