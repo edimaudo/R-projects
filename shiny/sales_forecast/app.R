3b52ea45601f42244ed9c2f5ab5bf599268c389d
Edima <edimaudo@gmail.com>
2021-06-09 19:59:53 -0400

    Update app.R

--- shiny/sales_forecast/app.R
+++ shiny/sales_forecast/app.R
@@ -25,6 +25,8 @@ df <- read.csv("volume_by_day_type_and_region.csv")
 #===============
 df$date <- lubridate::as_date(df$date) #update date
 df$revenue <- df$average_price*df$total_volume #revenue
+sales.xts <- xts(x = df$revenue, order.by = df$date) 
+
 
 #===============
 # Drop downs
@@ -46,7 +48,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("About", tabName = "about", icon = icon("dashboard")),
-      menuItem("Sales forecast", tabName = "forecast", icon = icon("th"))
+      menuItem("sales forecast", tabName = "forecast", icon = icon("th"))
     )
   ),
   dashboardBody(
@@ -58,7 +60,7 @@ ui <- dashboardPage(
                   selectInput("aggregateInput", "Aggregate", choices = aggregate_info, selected = 'weekly'),
                   selectInput("typeInput", "Type", choices = type_info,selected = "All"),
                   selectInput("regionInput", "Region", choices = region_info, selected = "All"),
-                  selectInput("forecasthorizonInput", "Forecast Horizon", choices = horizon_info, selected = 12),
+                  selectInput("horizonInput", "Forecast Horizon", choices = horizon_info, selected = 12),
                   selectInput("modelInput", "Model", 
                               choices = model_info, selected = 'auto exponential'),
                   submitButton("Submit")
@@ -86,6 +88,75 @@ server <- function(input, output,session) {
   
   #forecast output
   output$forecastPlot <- renderPlot({
+  
+    if(input$aggregateInput == 'weekly'){
+    sales.data <- apply.weekly(sales.xts, mean) 
+    sales.end <- floor(0.8*length(sales.data)) #select the first 80% of the data
+    sales.train <- sales.data[1:sales.end,] 
+    sales.test <- sales.data[(sales.end+1):length(sales.data),]
+    sales.start <- c(year (start(sales.train)), month(start(sales.train)),
+                       week(start(sales.train)))
+    sales.end <- c(year(end(sales.train)), month(end(sales.train)), 
+                     week(end(sales.train)))
+    sales.train <- ts(as.numeric(sales.train), start = sales.start, 
+                        end = sales.end, frequency = as.numeric(input$frequencyInput) )
+    sales.start <- c(year (start(sales.test)), month(start(sales.test)),
+                       week(start(sales.test)))
+    sales.end <- c(year(end(sales.test)), month(end(sales.test)), 
+                     week(end(sales.test)))
+    sales.test <- ts(as.numeric(sales.test), start = sales.start, 
+                       end = sales.end, frequency = as.numeric(input$frequencyInput))
+  } else {
+    sales.data <- apply.monthly(sales.xts, mean) 
+    sales.data <- apply.weekly(sales.xts, mean) 
+    sales.end <- floor(0.8*length(sales.data)) #select the first 80% of the data
+    sales.train <- sales.data[1:sales.end,] 
+    sales.test <- sales.data[(sales.end+1):length(sales.data),]
+    sales.start <- c(year (start(sales.train)), month(start(sales.train)))
+    sales.end <- c(year(end(sales.train)), month(end(sales.train)))
+    sales.train <- ts(as.numeric(sales.train), start = sales.start, 
+                        end = sales.end, frequency = as.numeric(input$frequencyInput) )
+    sales.start <- c(year (start(sales.test)), month(start(sales.test)))
+    sales.end <- c(year(end(sales.test)), month(end(sales.test)))
+    sales.test <- ts(as.numeric(sales.test), start = sales.start, 
+                       end = sales.end, frequency = as.numeric(input$frequencyInput))
+  }
+  
+  #set forecast horizon
+  forecast.horizon <- as.numeric(input$horizonInput)
+  
+  # models
+  if(input$modelInput == 'auto exponential'){
+    sales.train%>% 
+      ets %>% 
+      forecast(h=forecast.horizon) %>% 
+      plot()
+    lines(sales.test, col = "red")             
+  } else if (input$modelInput == 'auto arima'){
+    sales.train%>% 
+      auto.arima %>% 
+      forecast(h=forecast.horizon) %>% 
+      plot()
+    lines(sales.test, col = "red")             
+  } else if (input$modelInput == 'simple exponential'){
+    sales.train%>% 
+      HoltWinters(beta=FALSE, gamma=FALSE) %>% 
+      forecast(h=forecast.horizon) %>% 
+      plot()
+    lines(sales.test, col = "red")             
+  } else if (input$modelInput == 'double exponential'){
+    sales.train%>% 
+      HoltWinters(beta = TRUE, gamma=FALSE) %>% 
+      forecast(h=forecast.horizon) %>% 
+      plot()
+    lines(sales.test, col = "red") 
+  } else {
+    sales.train%>% 
+      HoltWinters(beta = TRUE, gamma = TRUE) %>% 
+      forecast(h=forecast.horizon) %>% 
+      plot()
+    lines(sales.test, col = "red")           
+  }
     
   })
   
