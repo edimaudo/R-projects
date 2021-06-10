30e9ae7320f833772f37baa8fb14a3360ef8ce01
Edima <edimaudo@gmail.com>
2021-06-10 06:55:18 -0400

    Update app.R

--- shiny/sales_forecast/app.R
+++ shiny/sales_forecast/app.R
@@ -155,42 +155,42 @@ server <- function(input, output,session) {
       
     }
     
+    
     #set forecast horizon
     forecast.horizon <- as.numeric(input$horizonInput)
     
     # models
     if(input$modelInput == 'auto exponential'){
-      patient.train%>% 
+      sales.train%>% 
         ets %>% 
         forecast(h=forecast.horizon) %>% 
         plot()
-      lines(patient.test, col = "red")             
+      lines(sales.test, col = "red")             
     } else if (input$modelInput == 'auto arima'){
-      patient.train%>% 
+      sales.train%>% 
         auto.arima %>% 
         forecast(h=forecast.horizon) %>% 
         plot()
-      lines(patient.test, col = "red")             
+      lines(sales.test, col = "red")             
     } else if (input$modelInput == 'simple exponential'){
-      patient.train%>% 
+      sales.train%>% 
         HoltWinters(beta=FALSE, gamma=FALSE) %>% 
         forecast(h=forecast.horizon) %>% 
         plot()
-      lines(patient.test, col = "red")             
+      lines(sales.test, col = "red")             
     } else if (input$modelInput == 'double exponential'){
-      patient.train%>% 
+      sales.train%>% 
         HoltWinters(beta = TRUE, gamma=FALSE) %>% 
         forecast(h=forecast.horizon) %>% 
         plot()
-      lines(patient.test, col = "red") 
+      lines(sales.test, col = "red") 
     } else {
-      patient.train%>% 
+      sales.train%>% 
         HoltWinters(beta = TRUE, gamma = TRUE) %>% 
         forecast(h=forecast.horizon) %>% 
         plot()
-      lines(patient.test, col = "red")           
+      lines(sales.test, col = "red")           
     }
-    
       
     
     
@@ -199,7 +199,97 @@ server <- function(input, output,session) {
   
   # accuracy output
   output$accuracyOutput <- DT::renderDataTable({
-  
+    # type ^ region info
+    if (input$typeInput == 'All' && input$regionInput == "All"){
+      df_output <- df %>%
+        group_by(date) %>%
+        summarise(Revenue = sum(revenue))    
+    } else if (input$typeInput == 'All' && input$regionInput != "All") {
+      df_output <- df %>%
+        group_by(date) %>%
+        filter(region  == input$regionInput) %>%
+        summarise(Revenue = sum(revenue))     
+    } else if (input$typeInput != 'All' && input$regionInput == "All") {
+      df_output <- df %>%
+        group_by(date) %>%
+        filter(type == input$typeInput) %>%
+        summarise(Revenue = sum(revenue))        
+    } else {
+      df_output <- df %>%
+        group_by(date) %>%
+        filter(type == input$typeInput,region  == input$regionInput) %>%
+        summarise(Revenue = sum(revenue))        
+    }
+    
+    
+    #aggregation
+    if(input$aggregateInput == 'weekly'){
+      sales.xts <- xts(x = df_output$Revenue, order.by = df_output$date) 
+      sales.data <- apply.weekly(sales.xts,mean)
+      sales.end <- floor(0.8*length(sales.data)) #select the first 80% of the data
+      sales.train <- sales.data[1:sales.end,] 
+      sales.test <- sales.data[(sales.end+1):length(sales.data),]
+      sales.start <- c(year (start(sales.train)), month(start(sales.train)),
+                       week(start(sales.train)))
+      sales.end <- c(year(end(sales.train)), month(end(sales.train)), week(end(sales.train)))
+      sales.train <- ts(as.numeric(sales.train), start = sales.start, 
+                        end = sales.end, frequency = as.numeric(input$frequencyInput) )
+      sales.start <- c(year (start(sales.test)), month(start(sales.test)),
+                       week(start(sales.test)))
+      sales.end <- c(year(end(sales.test)), month(end(sales.test)), week(end(sales.test)))
+      sales.test <- ts(as.numeric(sales.test), start = sales.start, 
+                       end = sales.end, frequency = as.numeric(input$frequencyInput))    
+    } 
+    else {
+      sales.xts <- xts(x = df$Revenue, order.by = df$date) 
+      sales.data <- apply.monthly(sales.xts, mean) 
+      sales.end <- floor(0.8*length(sales.data)) #select the first 80% of the data
+      sales.train <- sales.data[1:sales.end,] 
+      sales.test <- sales.data[(sales.end+1):length(sales.data),]
+      sales.start <- c(year (start(sales.train)), month(start(sales.train)),
+                       week(start(sales.train)))
+      sales.end <- c(year(end(sales.train)), month(end(sales.train)), week(end(sales.train)))
+      sales.train <- ts(as.numeric(sales.train), start = sales.start, 
+                        end = sales.end, frequency = as.numeric(input$frequencyInput) )
+      sales.start <- c(year (start(sales.test)), month(start(sales.test)),
+                       week(start(sales.test)))
+      sales.end <- c(year(end(sales.test)), month(end(sales.test)), week(end(sales.test)))
+      sales.test <- ts(as.numeric(sales.test), start = sales.start, 
+                       end = sales.end, frequency = as.numeric(input$frequencyInput))       
+      
+    }
+    
+    
+    #set forecast horizon
+    forecast.horizon <- as.numeric(input$horizonInput)
+    
+    # models
+    if(input$modelInput == 'auto exponential'){
+      patient.train.forecast <- ets(patient.train) %>% 
+        forecast(h=forecast.horizon)    
+    } else if (input$modelInput == 'auto arima'){
+      patient.train.forecast <- auto.arima(patient.train) %>% 
+        forecast(h=forecast.horizon)             
+    } else if (input$modelInput == 'simple exponential'){
+      patient.train.forecast <- HoltWinters(patient.train,
+                                            beta=FALSE, 
+                                            gamma=FALSE) %>% 
+        forecast(h=forecast.horizon)             
+    } else if (input$modelInput == 'double exponential'){
+      patient.train.forecast <- HoltWinters(patient.train,
+                                            beta=TRUE, 
+                                            gamma=FALSE) %>% 
+        forecast(h=forecast.horizon)  
+    } else {
+      patient.train.forecast <- HoltWinters(patient.train,
+                                            beta=TRUE, 
+                                            gamma=TRUE) %>% 
+        forecast(h=forecast.horizon)  
+    }
+    outputInfo <- as.data.frame(accuracy(sales.train.forecast,sales.test))
+    outputInfo    
+    
+    
   })
   
 }
