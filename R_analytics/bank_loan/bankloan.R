27ea12f577eb80d395ecf583303dce044ad77cc1
Edima <edimaudo@gmail.com>
2021-06-22 12:52:19 -0400

    Update bankloan.R

--- R_analytics/bank_loan/bankloan.R
+++ R_analytics/bank_loan/bankloan.R
@@ -6,7 +6,7 @@ rm(list = ls()) #clear environment
 # libraries
 packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
               ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
-              'gridExtra','doParallel','readxl')
+              'gridExtra','doParallel','readxl','doParallel')
 # load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -38,26 +38,35 @@ df <- na.omit(df)
 
 # calculate correlation
 correlationMatrix <- cor(df)
+corrplot(correlationMatrix,method='number')
 # # summarize the correlation matrix
-# print(correlationMatrix)
+print(correlationMatrix)
 # # find attributes that are highly corrected (ideally >0.75)
 highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.75)
 # # print indexes of highly correlated attributes
 print(highlyCorrelated)
+
+
+#tweak for sampling
+#check for imbalance
+#print(table(df_new$crime))
+# #weight due to 
+# model_weights <- ifelse(train$crime == "Assault",
+#                         (1/table(train$crime)[1]) * 0.5,
+#                         (1/table(train$crime)[2]) * 0.5)
 
 #normalize data
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
+#Label Encoder
+labelEncoder <-function(x){
+  as.numeric(factor(x))-1
+}
 
-
-
-
-
-
-
-
+df_cts <- df[,c(1,11,12,13,14,16,17,18,19,20)]
+df_cts <- as.data.frame(lapply(df_cts, normalize))
 
 
 #split into train and test
@@ -65,9 +74,11 @@ sample <- sample.split(df,SplitRatio = 0.75)
 train <- subset(df,sample ==TRUE)
 test <- subset(df, sample==FALSE)
 
-#--------------------
-#initial prediction
-#--------------------
+
+#model training
+cl <- makePSOCKcluster(4)
+registerDoParallel(cl)
+# Prediction model
 set.seed(123)
 #cross fold validation
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
@@ -80,61 +91,110 @@ fit.rf <- train(Output~., data=train, method="rf", trControl=control)
 #boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
 fit.gbm <- train(Output~., data=train, method="gbm", trControl=control)
 
-#------------------
-#compare models
-#------------------
-results <- resamples(list(logistic = fit.glm, svm = fit.svm, 
-                          randomforest = fit.rf, gradboost = fit.gbm))
-summary(results)
-
-#apply model on test data
-test_scores <- predict(fit.gbm, test)
-confusionMatrix(test_scores, test$Output)
-
-#create models
-#cross fold validation
-control <- trainControl(method="repeatedcv", number=10, repeats=3)
-#logistic regression
-fit.glm <- train(Output~., data=train, method="glm", trControl=control)
-#svm
-fit.svm <- train(Output~., data=train, method="svmRadial", trControl=control)
+fit.glm <- train(as.factor(crime)~., data=train, method="glm",family=binomial(),
+                 metric = "Accuracy", trControl = control)
 #random forest
-fit.rf <- train(Output~., data=train, method="rf", trControl=control)
+fit.rf <- train(as.factor(crime)~., data=train, method="rf", 
+                metric = "Accuracy", trControl = control)
 #boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
-fit.gbm <- train(Output~., data=train, method="gbm", trControl=control)
+fit.gbm <- train(as.factor(crime)~., data=train, method="gbm", 
+                 metric = "Accuracy", trControl = control)
+#svm
+fit.svm <- train(as.factor(crime)~., data=train, method="svmRadial", 
+                 metric = "Accuracy", trControl = control)
+#nnet
+fit.nnet <- train(as.factor(crime)~., data=train, method="nnet", 
+                  metric = "Accuracy", trControl = control)
+#naive
+fit.naive <- train(as.factor(crime)~., data=train, 
+                   method="naive_bayes", metric = "Accuracy", 
+                   trControl = control)
+#extreme gradient boosting
+fit.xgb <- train(as.factor(crime)~., data=train, 
+                 method="xgbTree", metric = "Accuracy", trControl = control)
+#bagged cart
+fit.bg <- train(as.factor(crime)~., data=train, 
+                method="treebag", metric = "Accuracy", trControl = control)
+#decision tree
+cl <- makePSOCKcluster(4)
+registerDoParallel(cl)
+fit.dtree <- train(as.factor(crime)~., data=train, 
+                   method="C5.0", metric = "Accuracy", trControl = control)
+#knn
+fit.knn <- train(as.factor(crime)~., data=train, 
+                 method="kknn", metric = "Accuracy", trControl = control)
+#ensemble
+fit.ensemble <- train(as.factor(crime)~., data=train, 
+                      method="nodeHarvest", metric = "Accuracy", trControl = control)
+
+stopCluster(cl)
 
 #------------------
 #compare models
 #------------------
-results <- resamples(list(logistic = fit.glm, svm = fit.svm, randomforest = fit.rf, gradboost = fit.gbm))
-summary(results)
+results <- resamples(list(randomforest = fit.rf, 
+                          `gradient boost` = fit.gbm, 
+                          `support vector machine` = fit.svm,
+                          baggedCart = fit.bg, 
+                          neuralnetwork = fit.nnet,
+                          xgboost = fit.xgb, 
+                          logisticregression = fit.glm, 
+                          `decision tree` = fit.dtree, 
+                          `naive bayes` = fit.naive,
+                          `ensemble` = fit.ensemble, 
+                          `knn` = fit.knn))
 
+summary(results)
+# boxplot comparison
+bwplot(results)
+# Dot-plot comparison
+dotplot(results)
+#logistic regression
+logistic_regression_model <- nnet::multinom(crime ~ ., 
+                                            data = crimes_data_model2 %>% 
+                                              select(Year, Age,`Job_House Wife`,`Job_Maid`,
+                                                     `Marital Status_Single`,
+                                                     Nationality_UAE, Target))
+summary(logistic_regression_model)
 #use of test data
 test_scores <- predict(fit.gbm, test)
 confusionMatrix(test_scores, test$Output)
 
+# Make predictions
+predicted.classes <- fit.knn %>% predict(test)
+output <- confusionMatrix(data = predicted.classes, reference = test$crime, mode = "everything")
+output
+#other metrics
 
-#regression modelling
+caret::varImp(fit.rf)
 
-fit <- lm(Price ~., data=training)
-summary(fit)
-plot(Price ~., data=training)
-plot(residuals(fit) ~ ., data=training)
+#plot confusion matrix of selected option
+library(ggplot2)     # to plot
+library(gridExtra)   # to put more
+library(grid)        # plot together
 
-pred1 <- predict(fit, newdata = test)
-rmse <- sqrt(sum((exp(pred1) - test$medv)^2)/length(test$medv))
-c(RMSE = rmse, R2=summary(fit)$r.squared)
+# plotting the matrix
+output2 <- as.data.frame(output$table)
+colnames(output2) <- c("Predicted",'Actual',"Freq")
+cm_d_p <-  ggplot(data =output2, aes(x = Predicted , y =  Actual, fill = Freq))+
+  geom_tile() +
+  geom_text(aes(label = paste("",Freq)), color = 'white', size = 8) +
+  theme_light() +
+  guides(fill=FALSE) 
+
+cm_d_p
 
 
 
 
-#data to keep
-keep <- c("age","day","balance","campaign","pdays","job_entrepreneur","job_blue-collar","job_unknown","job_retired","job_admin.","job_services",          "job_self-employed","job_unemployed","job_housemaid","job_student","marital_single","marital_divorced","education_secondary","education_primary","default_yes","housing_no","loan_yes","contact_cellular","contact_telephone","month_jun","month_jul", "month_aug","month_oct","month_nov","month_dec","month_jan","month_feb","month_mar","month_apr","month_sep","poutcome_failure","poutcome_other","poutcome_success","y_yes")          
-final <- dummy_data[keep]
 
-#split data
-set.seed(123)
 
-sample <- sample.split(final,SplitRatio = 0.75)
-training <- subset(final,sample ==TRUE)
-test <- subset(final, sample==FALSE)
+
+
+
+
+
+
+
+
+
