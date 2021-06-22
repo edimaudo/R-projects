647b38d553f0002949559883a66f1379ada56f2e
Edima <edimaudo@gmail.com>
2021-06-22 10:50:19 -0400

    Add files via upload

---
+++ R_analytics/bank_loan/bankloan.R
@@ -0,0 +1,16 @@
+#===================
+## Load Libraries
+#===================
+rm(list = ls()) #clear environment
+
+# libraries
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
+              ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
+              'gridExtra','doParallel','readxl')
+# load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
