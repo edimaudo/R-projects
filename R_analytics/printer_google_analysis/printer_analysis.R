0ad328c3a9bf62c8cf1abd7df880fdb86259ecaa
Edima <edimaudo@gmail.com>
2021-07-10 12:24:29 -0400

    Update printer_analysis.R

--- R_analytics/printer_google_analysis/printer_analysis.R
+++ R_analytics/printer_google_analysis/printer_analysis.R
@@ -2,15 +2,45 @@ rm(list = ls()) #clear environment
 #=============
 # Packages
 #=============
-packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies",'readxl',
-              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
-              'forecast','TTR','xts','lubridate')
-#=============
-# load packages
-#=============
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret",'readxl',
+              'scales','dplyr','mlbench','caTools','wordcloud2','gridExtra',
+              'tidytext','stringr','reshape2',"tm", "SnowballCC", "RColorBrewer", 
+              "biclust", "cluster", "igraph", "fpc",'forecast','TTR','xts','lubridate')
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package)
     library(package, character.only=T)
   }
 }
+
+#=============
+# Load data
+#=============
+
+df <- read_excel(file.choose())
+
+glimpse(df)
+
+df.backup <- df #backup
+
+#=============
+# Text analysis
+#=============
+
+# function to expand contractions in an English-language source
+fix.contractions <- function(doc) {
+  # "won't" is a special case as it does not expand to "wo not"
+  doc <- gsub("won't", "will not", doc)
+  doc <- gsub("can't", "can not", doc)
+  doc <- gsub("n't", " not", doc)
+  doc <- gsub("'ll", " will", doc)
+  doc <- gsub("'re", " are", doc)
+  doc <- gsub("'ve", " have", doc)
+  doc <- gsub("'m", " am", doc)
+  doc <- gsub("'d", " would", doc)
+  # 's could be 'is' or could be possessive: it has no expansion
+  doc <- gsub("'s", "", doc)
+  return(doc)
+}
+
+
