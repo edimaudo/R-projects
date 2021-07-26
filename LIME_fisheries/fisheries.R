ca1294abbebe9f7a9544fc1dd06d0100354b90b1
Edima <edimaudo@gmail.com>
2021-07-26 14:41:31 -0400

    Delete LIME_fisheries directory

--- LIME_fisheries/fisheries.R
+++
@@ -1,5 +0,0 @@
-#LIME installation
-#install.packages("devtools", repos='http://cran.us.r-project.org')
-#devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
-#devtools::install_github("merrillrudd/LIME", dependencies=TRUE)
-
