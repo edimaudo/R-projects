2181f6dade3242cd7f81bce674911ec9c59f9212
Edima <edimaudo@gmail.com>
2021-07-26 14:41:48 -0400

    Add files via upload

---
+++ R_analytics/LIME_fisheries/fisheries.R
@@ -0,0 +1,5 @@
+#LIME installation
+#install.packages("devtools", repos='http://cran.us.r-project.org')
+#devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+#devtools::install_github("merrillrudd/LIME", dependencies=TRUE)
+
