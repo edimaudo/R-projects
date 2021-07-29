2e813779158500ac3486dd30905c8f414d2a8d19
Edima <edimaudo@gmail.com>
2021-07-29 17:54:00 -0400

    Update fisheries.R

--- R_analytics/LIME_fisheries/fisheries.R
+++ R_analytics/LIME_fisheries/fisheries.R
@@ -23,17 +23,20 @@ for (package in packages) {
 #######################################
 # Load data
 #######################################
-df<- read.csv(file.choose(), header=TRUE)
+df <- read_csv(file.choose())
+df<- sapply(df, as.integer)
+df <- as.data.frame(df)
 #######################################
 # LH information
 #######################################
-# single fleet
-lh <- create_lh_list(vbk = 1.50, linf = 24.5, t0 = 0, lwa = 0.00407, lwb = 3.16,
-                     S50 = c(5.66), S95 = c(7.98), selex_input = "length", selex_type = c("logistic"),
-                     M50 = 14.76, maturity_input = "length", M = 2.41, binwidth = 1, CVlen = 0.1,
-                     SigmaR = 0.737, SigmaF = 0.3, SigmaC = 0.1, SigmaI = 0.1, R0 = 1, Frate = 0.1,
-                     Fequil = 0.25, qcoef = 1e-05, start_ages = 0, rho = 0.43, theta = 10, nseasons = 1,
-                     nfleets = 1)
+# 1 fleets
+lh <- create_lh_list(vbk = 0.94, linf = 57.84, t0 = -0.01, lwa = 0.023, lwb = 2.81, 
+                     S50 = c(20.84), S95 = c(30), selex_input = "length", 
+                     selex_type = c("logistic"), M50 = 30, maturity_input = "length", 
+                     M = 1.033, binwidth = 1, CVlen = 0.1, SigmaR = 0.737, SigmaF = 0.2, 
+                     SigmaC = 0.1, SigmaI = 0.1, R0 = 1, Frate = 0.1, Fequil = 0.25, 
+                     qcoef = 1e-05, start_ages = 0, rho = 0.43, theta = 10, nseasons = 1,
+                                   nfleets = 1)
 
 # lh plots
 par(mfrow=c(2,2), mar=c(4,4,3,1))
@@ -42,88 +45,127 @@ plot(lh$W_a, type="l", lwd=4, xlab="Age", ylab="Weight (g)")
 plot(lh$Mat_l, type="l", lwd=4, xlab="Length (cm)", ylab="Proportion mature")
 # plot selectivity for the first (and only) fleet (first row)
 plot(lh$S_fl[1,], type="l", lwd=4, xlab="Length (cm)", ylab="Proportion selected to gear")
+
 #######################################
-# Data setup
+## Data prep
 #######################################
+df$Fleet<- rowSums(df[ , c(2:13)], na.rm=TRUE)
+df <- df[,c(1,14)]
+df<- sapply(df, as.integer)
+df <- as.data.frame(df)
+
 ## identify length bins
 bins <- df[,1]
 
 ## setup length frequency matrix
 lf <- matrix(df[,2], nrow=1, ncol=nrow(df))
-
 rownames(lf) <- "2018"
 colnames(lf) <- bins
 #######################################
-# RUN LIME
+## Run LIME
 #######################################
 ## Single year only
 data_list <- list("years"=2018, "LF"=lf)
-
 input_data <- create_inputs(lh=lh, input_data=data_list)
+inputs_LC <- create_inputs(lh=lh, input_data=data_list)
+inputs_all <- create_inputs(lh=lh, input_data=data_list)
 
 LFlist <- list()
 LFlist[[1]] <- matrix(input_data$LF[,,1], nrow=length(input_data$years))
-colnames(LFlist[[1]]) <- input_data$highs
+colnames(LFlist[[1]]) <- df$Lengthclass
 rownames(LFlist[[1]]) <- input_data$years
 
 LFdf <- LFreq_df(LFlist)
-
 plot_LCfits(LF_df=LFdf)
 
-res <- run_LIME(modpath=NULL, input=input_data, data_avail="LC", Rdet=TRUE)
+#######################################
+## Length-data only
+#######################################
+lc_only <- run_LIME(modpath=NULL, 
+                   input=inputs_LC,
+                   data_avail="LC")
 
 ## check TMB inputs
-Inputs <- res$Inputs
+Inputs <- lc_only$Inputs
 
 ## Report file
-Report <- res$Report
+Report <- lc_only$Report
 
 ## Standard error report
-Sdreport <- res$Sdreport
+Sdreport <- lc_only$Sdreport
 
 ## check convergence
 hessian <- Sdreport$pdHess
-gradient <- res$opt$max_gradient <= 0.001
+gradient <- lc_only$opt$max_gradient <= 0.001
 hessian == TRUE & gradient == TRUE
 
-## Examine the output
-## SPR
-spr <- Report$SPR_t
+## hessian not positive definite -- the following line helps 
+# diagnose which parameters can't be estimated 
+check <- TMBhelper::Check_Identifiable(lc_only$obj)
 
-## standard error
-sd_spr <- summary(Sdreport)[which(rownames(summary(Sdreport))=="SPR_t"),2]
+## issues estimating F - try a more narrow penalty on F
+inputs_LC_new <- inputs_LC
+inputs_LC_new$SigmaF <- 0.1
 
-## lower confidence limit
-lcl_spr <- max(0,spr - 1.96 * sd_spr)
+lc_only2 <- run_LIME(modpath=NULL, 
+                     input=inputs_LC_new,
+                     data_avail="LC")
 
-## upper confidence limit
-ucl_spr <- spr + 1.96 * sd_spr
+## check TMB inputs
+Inputs <- lc_only2$Inputs
 
-save <- data.frame("model"="LIME", "run"="singleyear", "spr"=spr, 
-                   "lcl_spr"=lcl_spr, "ucl_spr"=ucl_spr)
+## Report file
+Report <- lc_only2$Report
 
-F50 <- with(lh, uniroot(f=calc_ref, lower=0, upper=3, ages=ages, 
-                        Mat_a=Mat_a, W_a=W_a, M=M, S_fa=Report$S_fa, ref=0.5))$root
-Report$F_y/F50
-(1-spr)/(1-F50)
+## Standard error report
+Sdreport <- lc_only2$Sdreport
 
-Report$D_t
+## check convergence
+hessian <- Sdreport$pdHess
+gradient <- lc_only$opt$max_gradient <= 0.001
+hessian == TRUE & gradient == TRUE
 
+## LBSPR
+LB_pars <- new("LB_pars")
+LB_pars@MK <- inputs_all$M/inputs_all$vbk
+LB_pars@Linf <- inputs_all$linf
+LB_pars@L50 <- inputs_all$ML50
+LB_pars@L95 <- inputs_all$ML95
+LB_pars@Walpha <- inputs_all$lwa
+LB_pars@Wbeta <- inputs_all$lwb
+LB_pars@R0 <- inputs_all$R0
+LB_pars@Steepness <- ifelse(inputs_all$h==1, 0.99, inputs_all$h)
+LB_pars@BinWidth <- inputs_all$binwidth
 
-## plot length composition data and fits
+LB_lengths <- new("LB_lengths")
+LB_lengths@LMids <- inputs_all$mids
+LB_lengths@LData <- t(matrix(inputs_all$LF, ncol=length(inputs_all$mids)))
+LB_lengths@Years <- as.numeric(rownames(inputs_all$LF))
+LB_lengths@NYears <- ncol(LB_lengths@LData)
+
+lbspr <- LBSPRfit(LB_pars=LB_pars, LB_lengths=LB_lengths)
+
+## plot length composition data
 plot_LCfits(Inputs=Inputs, 
-            Report=Report)
+            Report=Report,
+            LBSPR=lbspr)		
 
-abline(v=lh$linf, col="red", lwd=2, lty=2)		
+## plot model output -- not working due to InputMat[, ]
+# plot_output(Inputs=Inputs, 
+#             Report=Report,
+#             Sdreport=Sdreport, 
+#             lh=lh,
+#             True=true, 
+#             LBSPR=lbspr,
+#             plot=c("Fish","Rec","SPR","ML","SB","Selex"), 
+#             set_ylim=list("Fish" =c(0,1), "SPR" = c(0,1), "SB"=c(0,2)))	
 
-## plot model output
+# This works
 plot_output(Inputs=Inputs, 
             Report=Report,
             Sdreport=Sdreport, 
             lh=lh,
-            plot="Selex")
-abline(v=lh$linf, lwd=2, lty=2, col="red")
-abline(v=Report$ML_ft[length(Report$ML_ft)], lwd=2, lty=2, col="blue")
-legend("topleft", legend=c("Selectivity", "Mean length in catch", "Linf"), 
-       col=c("#00AA00", "blue", "red"), lty=c(1,2,2), lwd=2)
-
+            True=true, 
+            LBSPR=lbspr,
+            plot=c("Selex"), 
+            set_ylim=list("Fish" =c(0,1), "SPR" = c(0,1), "SB"=c(0,2)))	
