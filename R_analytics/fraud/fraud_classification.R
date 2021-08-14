c6bf040fe2500335e91efc854c07f098648dbb97
Edima <edimaudo@gmail.com>
2021-08-14 06:47:29 -0400

    Add files via upload

---
+++ R_analytics/fraud/fraud_classification.R
@@ -0,0 +1,14 @@
+#===================
+## Load Libraries
+#===================
+rm(list = ls()) #clear environment
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
+              ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
+              'gridExtra','doParallel','readxl')
+# load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
