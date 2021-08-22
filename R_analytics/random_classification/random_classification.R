1106ab458e1557a357b2925a7a0a1fbad6b564c9
Edima <edimaudo@gmail.com>
2021-08-22 18:21:42 -0400

    Add files via upload

---
+++ R_analytics/random_classification/random_classification.R
@@ -0,0 +1,22 @@
+# Classify Y value
+
+#===================
+## Load Libraries
+#===================
+rm(list = ls()) #clear environment
+
+# Packages
+packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
+              ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
+              'gridExtra','doParallel','readxl','gridExtra','grid')
+
+# Load packages
+for (package in packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    install.packages(package)
+    library(package, character.only=T)
+  }
+}
+
+
+# LO
