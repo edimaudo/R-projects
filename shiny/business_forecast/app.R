ec6232475d0eefdf539dab14f1cb3891416c9e02
Edima <edimaudo@gmail.com>
2021-09-26 13:10:41 -0400

    Update app.R

--- shiny/business_forecast/app.R
+++ shiny/business_forecast/app.R
@@ -26,6 +26,7 @@ week_info <- read_excel("Forecast Data.xlsx",sheet="WeekInfo")
 #=============
 # Forecast Inputs
 #=============
+segment_info <- c("Profit","Turnover","Customer Count")
 horizon_info <- c(1:50) #default 14
 frequency_info <- c(7, 12, 52, 365)
 difference_info <- c("Yes","No")
@@ -34,5 +35,98 @@ model_info <- c('auto-arima','auto-exponential','simple-exponential',
                 'double-exponential','triple-exponential', 'tbat')
 
 #=============
-# Define UI for application
+# Application UI
 #=============
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Business Forecast"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Analysis", tabName = "analysis", icon = icon("th")),
+            menuItem("Forecasting", tabName = "Forecast", icon = icon("th"))
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "analysis",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput("segmentInput", "Segment", 
+                                        choices = segment_info, selected = "Profit"),
+                            selectInput("frequencyInput", "Frequency", 
+                                        choices = frequency_info, selected = 7),
+                            radioButtons("differenceInput","Difference",
+                                         choices = difference_info, selected = "No"),
+                            numericInput("differenceNumericInput", "Difference Input", 
+                                         1, min = 1, max = 52, step = 0.5),
+                            radioButtons("logInput","Log",
+                                         choices = log_info, selected = "No"),
+                            submitButton("Submit")
+                        ),
+                        mainPanel(
+                            h1("Analysis",style="text-align: center;"), 
+                            tabsetPanel(type = "tabs",
+                                        tabPanel(
+                                            h4("Decomposition",
+                                               style="text-align: center;"),
+                                            plotOutput("decompositionPlot")),
+                                        tabPanel(
+                                            h4("Multi seasonal Decomposition",
+                                               style="text-align: center;"),
+                                            plotOutput("multidecompositionPlot")),
+                                        tabPanel(
+                                            h4("ACF Plot",style="text-align: center;"), 
+                                            plotOutput("acfPlot")),
+                                        tabPanel(
+                                            h4("PACF Plot",style="text-align: center;"), 
+                                            plotOutput("pacfPlot"))
+                            )
+                        )
+                    )  
+            ),
+            tabItem(tabName = "Forecast",
+                    sidebarLayout(
+                        sidebarPanel(
+                            selectInput("segmentInput", "Segment", 
+                                        choices = segment_info, selected = "Profit"),
+                            selectInput("horizonInput", "Horizon", 
+                                        choices = horizon_info, selected = 14),
+                            selectInput("frequencyInput", "Frequency", 
+                                        choices = frequency_info, selected = 7),
+                            sliderInput("traintestInput", "Train/Test Split",
+                                        min = 0, max = 1,value = 0.8),
+                            #checkboxGroupInput("modelInput", "Models",choices = model_info, 
+                            #                   selected = model_info),
+                            #sliderInput("autoInput", "Auto-regression",
+                            #            min = 0, max = 100,value = 0),
+                            #sliderInput("difference2Input", "Difference",
+                            #            min = 0, max = 52,value = 0),
+                            #sliderInput("maInput", "Moving Average",
+                            #            min = 0, max = 100,value = 0),
+                            submitButton("Submit")
+                        ),
+                        mainPanel(
+                            h1("Forecast Analysis",style="text-align: center;"), 
+                            tabsetPanel(type = "tabs",
+                                        tabPanel(h4("Forecast Visualization",style="text-align: center;"), 
+                                                 plotOutput("forecastPlot")),
+                                        tabPanel(h4("Forecast Results",style="text-align: center;"), 
+                                                 DT::dataTableOutput("forecastOutput")),
+                                        tabPanel(h4("Forecast Accuracy",style="text-align: center;"), 
+                                                 DT::dataTableOutput("accuracyOutput"))
+                            )
+                        )
+                    )
+            ) 
+        )
+    ) 
+) 
+
+#=============
+# Server logic 
+#=============
+server <- function(input, output,session) {
+    
+}
+
+shinyApp(ui, server)
