56e389c9d43fcf71d4d88bb5a06f2d53f5d25b44
Edima <edimaudo@gmail.com>
2021-12-30 10:57:18 -0500

    Add files via upload

---
+++ shiny/denso/app.R
@@ -0,0 +1,17 @@
+#=============
+# Denso Sales Analysis
+#=============
+
+rm(list = ls()) #clear environment
+#=============
+# Packages
+#=============
+packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
+              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
+              'forecast','lubridate')
+for (package in packages) {
+    if (!require(package, character.only=T, quietly=T)) {
+        install.packages(package)
+        library(package, character.only=T)
+    }
+}
