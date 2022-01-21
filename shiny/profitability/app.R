358d09ec9a93d2fe3b776c1f190de195ee35331b
Edima <edimaudo@gmail.com>
2022-01-20 20:03:24 -0500

    Update app.R

--- shiny/profitability/app.R
+++ shiny/profitability/app.R
@@ -30,6 +30,7 @@ df$DateLogged <- as.Date(df$DateLogged, format =  "%d/%m/%Y")
 # UI dropdown
 #----------------
 date_info <- c(sort(unique(df$DateLogged)))
+customer_info <- c('All',sort(unique(df$Customer)))
 
 ui <- dashboardPage(skin = "yellow",
         dashboardHeader(title = "Platform analysis"),
@@ -46,6 +47,7 @@ ui <- dashboardPage(skin = "yellow",
                     tabItem(tabName = "dashboard",
                             sidebarLayout(
                                 sidebarPanel(
+                                    selectInput("customerInput", "Customer", choices = customer_info),
                                     sliderInput("dateInput","Dates",min=min(date_info),
                                                 max=max(date_info),
                                                 value = c(min(date_info),max(date_info)),
@@ -53,7 +55,16 @@ ui <- dashboardPage(skin = "yellow",
                                   ),
                                 mainPanel(
                                     h2("Insights",style="text-align: center; font-style: bold;"), 
-                                    box(plotOutput("jobStatusPlot")),
+                                    tabBox(
+                                        title = "Profitability",
+                                        id = "tabset1", 
+                                        width = "100%",
+                                        selected = "Sales",
+                                        tabPanel("Sales", plotOutput("salesPlot", height = 150)),
+                                        tabPanel("Revenue", plotOutput("revenuePlot", height = 150))
+                                    ),
+                                    plotOutput("jobStatusPlot", height = 250),
+                                    
 
                                     
                                 )
@@ -70,7 +81,21 @@ server <- function(input, output, session) {
     
     # Job status plot
     output$jobStatusPlot <- renderPlot({
+        job_status_df <- df %>%
+            filter(DateLogged >= input$dateInput[1] & DateLogged <= input$dateInput[2]) %>%
+            group_by(Status) %>%
+            summarise(Total = n()) %>%
+            select(Status,Total)
         
+        ggplot(job_status_df, aes(reorder(Status,Total),Total),color = Status) + 
+            geom_bar(stat="identity",position="dodge",size=2 ,width = 0.4, aes(fill = Status)) + 
+            coord_flip() +theme_minimal() + scale_y_continuous(labels = comma) +
+            labs(x = "Status", y = "Total") + 
+            theme(legend.text = element_text(size = 12),
+                  legend.title = element_text(size = 15),
+                  axis.title = element_text(size = 12),
+                  axis.text = element_text(size = 12),
+                  axis.text.x = element_text(angle = 00, hjust = 1))
     })
     
     
