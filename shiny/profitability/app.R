c9907a4f7d70fec8d1b72830c3ce0ef3fca57ed2
Edima <edimaudo@gmail.com>
2022-01-20 19:38:40 -0500

    Update app.R

--- shiny/profitability/app.R
+++ shiny/profitability/app.R
@@ -31,7 +31,42 @@ df$DateLogged <- as.Date(df$DateLogged, format =  "%d/%m/%Y")
 #----------------
 date_info <- c(sort(unique(df$DateLogged)))
 
-
+ui <- dashboardPage(skin = "yellow",
+                    dashboardHeader(title = "Platform analysis"),
+                    dashboardSidebar(
+                        sidebarMenu(
+                            menuItem("Summary", tabName = "summary", icon = icon("th"))
+                        )
+                    ),
+                    dashboardBody(
+                        tabItems(
+                            #----------------
+                            # Summary
+                            #----------------
+                            tabItem(tabName = "summary",
+                                    fluidRow(
+                                        h2("Summary Insights",style="text-align: center;"),
+                                        tabBox(
+                                            title="Categories",
+                                            id = "tabset1",
+                                            width = "10%",
+                                            selected = "Price",
+                                            tabPanel("Price", plotOutput("categoryPricePlot",height = 250)),
+                                            tabPanel("Ratings", plotOutput("categoryRatingPlot",height = 250)),
+                                            tabPanel("Sales", plotOutput("categorySalesPlot",height = 250))
+                                        ),
+                                        tabBox(
+                                            title="Sub-Categories",
+                                            id = "tabset2",
+                                            width = "10%",
+                                            selected = "Price",
+                                            tabPanel("Price", plotOutput("subcategoryPricePlot", height = 250)),
+                                            tabPanel("Ratings", plotOutput("subcategoryRatingPlot", height = 250)),
+                                            tabPanel("Sales", plotOutput("subcategorySalesPlot", height = 250))
+                                            
+                                        )
+                                    )
+                            ),
 
 
 ##################
