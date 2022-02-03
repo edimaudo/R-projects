e382048e9a2d4ee5b3a34b681d27ecb9297d7ee5
Edima <edimaudo@gmail.com>
2022-02-03 05:53:16 -0500

    Add files via upload

--- shiny/app.R
+++ shiny/app.R
@@ -1,38 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
-################
-# Packages
-################
-rm(list = ls()) #clear environment
-packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
-              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
-              'forecast','lubridate')
-for (package in packages) {
-  if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package)
-    library(package, character.only=T)
-  }
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
 }
 
-################
-# Load data
-################
-df <- read_excel("otf.xlsx")
-
-
-################
-# Application UI
-################
-#------------------
-# UI drop-downs
-#------------------
-
-
-
-################
-# Server
-################
-server <- function(input, output,session) {
-  
-}
-
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
