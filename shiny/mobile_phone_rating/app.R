acd996d65094ce642241fe4c840f882785d4f8aa
Edima <edimaudo@gmail.com>
2022-02-20 14:03:29 -0500

    Add files via upload

---
+++ shiny/mobile_phone_rating/app.R
@@ -0,0 +1,16 @@
+#=========
+# Mobile Phone Ratings
+#=========
+
+packages <- c('ggplot2', 'corrplot','tidyverse','readxl',
+              'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
+              'forecast','TTR','xts','lubridate','plotly')
+
+for (package in packages) {
+    if (!require(package, character.only=T, quietly=T)) {
+        install.packages(package)
+        library(package, character.only=T)
+    }
+}
+
+df <- read.csv("mobile phone rating.csv")
