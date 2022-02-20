6c2e74b5154b660d6e3d4d3e78fd19c74196a983
Edima <edimaudo@gmail.com>
2022-02-20 15:12:45 -0500

    Update app.R

--- shiny/mobile_phone_rating/app.R
+++ shiny/mobile_phone_rating/app.R
@@ -21,16 +21,48 @@ df <- read.csv("mobile phone rating.csv")
 #----------
 # UI Dropdown
 #----------
-
+model <- sort(c(unique(df$model)))
 
 #=========
 # UI
 #=========
-
-
+ui <- dashboardPage(
+    dashboardHeader(title = "Mobile phone rating"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Phone Ratings", tabName = "rating", icon = icon("dashboard")),
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "rating",
+                    sidebarLayout(
+                        sidebarPanel(
+                            sliderInput("Years", "Years:", min = 1999, max = 2019, 
+                                        step=1, ticks = FALSE, sep="")
+                        ),
+                        mainPanel(
+                            fluidRow(
+                                h2("Grants and Amount Awarded",style="text-align: center;"),
+                                plotOutput("grantAwarded")
+                            )
+                    )
+                
+            )
+        )
+    )
+  )
+)
 #=========
 # Server
 #=========
-server <- function(input, output,session) {}
+server <- function(input, output,session) {
+    
+}
 
 shinyApp(ui, server)
+
+
+fluidRow(
+    infoBoxOutput("yearInfo"),
+    infoBoxOutput("grantInfo")
