295b57dad83d7a39b2b01a49cb8d2b3d20d09fa3
Edima <edimaudo@gmail.com>
2022-03-13 09:05:26 -0400

    Update app.R

--- shiny/printer_google_analysis/app.R
+++ shiny/printer_google_analysis/app.R
@@ -1,11 +1,12 @@
 # User content sentiment analysis
 rm(list = ls()) #clear environment
 
+#=============
 # libraries
+#=============
 packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies",'readxl',
               'shiny','shinydashboard','scales','dplyr','mlbench','caTools',
               'forecast','TTR','xts','lubridate')
-# load packages
 for (package in packages) {
     if (!require(package, character.only=T, quietly=T)) {
         install.packages(package)
