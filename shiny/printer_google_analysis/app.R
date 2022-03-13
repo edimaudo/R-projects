7791f0f9d7df4094440babb2c42ce46842e28bab
Edima <edimaudo@gmail.com>
2022-03-13 16:13:30 -0400

    Update app.R

--- shiny/printer_google_analysis/app.R
+++ shiny/printer_google_analysis/app.R
@@ -43,6 +43,35 @@ df$Product <- ifelse(df$appId == "com.hp.printercontrol", 'HP',
 printer_info <- c('Canon','Epson','Epson-Smart','HP')
 score_info <- c(1,2,3,4,5)
 
+#=============
+# Text analytics
+#=============
+# function to expand contractions in an English-language source
+fix.contractions <- function(doc) {
+    # "won't" is a special case as it does not expand to "wo not"
+    doc <- gsub("won't", "will not", doc)
+    doc <- gsub("can't", "can not", doc)
+    doc <- gsub("n't", " not", doc)
+    doc <- gsub("'ll", " will", doc)
+    doc <- gsub("'re", " are", doc)
+    doc <- gsub("'ve", " have", doc)
+    doc <- gsub("'m", " am", doc)
+    doc <- gsub("'d", " would", doc)
+    # 's could be 'is' or could be possessive: it has no expansion
+    doc <- gsub("'s", "", doc)
+    return(doc)
+}
+
+# function to remove special characters
+removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]", " ", x)
+# fix (expand) contractions
+df$Review <- sapply(df$Review, fix.contractions)
+# remove special characters
+df$Review <- sapply(df$Review, removeSpecialChars)
+# convert everything to lower case
+df$Review <- sapply(df$Review, tolower)
+remove_keywords <- c("printer","print", "printing","app")
+
 #--------------
 # UI
 #--------------
@@ -286,11 +315,8 @@ server <- function(input, output,session) {
                     legend.title = element_text(size = 10),
                     axis.title = element_text(size = 15),
                     axis.text = element_text(size = 15), 
-                    
                 )
-        
         }
-        
     })
     
 }
