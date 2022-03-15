02bd970640d9a676ad1c3691f09429f5296f13eb
Edima <edimaudo@gmail.com>
2022-03-14 20:28:18 -0400

    Update mtc.R

--- R_analytics/mtc/mtc.R
+++ R_analytics/mtc/mtc.R
@@ -6,7 +6,7 @@ rm(list = ls()) #clear environment
 # libraries
 packages <- c('ggplot2', 'corrplot','tidyverse',"caret","dummies","fastDummies"
               ,'FactoMineR','factoextra','readxl','scales','dplyr','mlbench','caTools',
-              'gridExtra','doParallel','readxl')
+              'gridExtra','doParallel','readxl','grid','gridExtra')
 # load packages
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -55,59 +55,63 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
-crime_info <- crimes_data_model %>%
-  select(Year, Age)
+df_other <- df %>%
+  select(UM_FISICA_ID,label)
 
-crime_info2 <- crimes_data_model %>%
-  select(Gender, `Marital Status`, `Education Level`, Religion,Job, Emirate,Nationality)
+df_cts <- df %>%
+  select(CANTIDAD_FISICA)
 
-crime <- crimes_data_model %>%
-  select(Target)
+df_cat<- df %>%
+  select(CODIGO_CERTIFICADO,NOMBRE_PROVEEDOR,NOMBRE_EQUIPO,
+         MODELO, PARTIDA_ARANCELARIA,MARCA_EQUIPO,FACTURA)
 
 #combine data
-df_cts <- as.data.frame(lapply(crime_info, normalize))
-df_cat <- as.data.frame(lapply(crime_info2, labelEncoder))
-df_new <- cbind(df_cts,df_cat,crime)
+df_cts <- as.data.frame(lapply(df_cts, normalize))
+df_cat <- as.data.frame(lapply(df_cat, labelEncoder))
+df_new <- cbind(df_cts,df_cat,df_other)
 
-colnames(df_new) <- c("Year","Age","Gender","Marital.Status","Education.Level",
-                      'Religion','Job','Emirate','Nationality','crime')
+#create train and test data
+set.seed(2020)
+sample <- sample.split(df_new,SplitRatio = 0.75)
+train <- subset(df_new,sample ==TRUE)
+test <- subset(df_new, sample==FALSE)
 
 #model training
 cl <- makePSOCKcluster(4)
 registerDoParallel(cl)
 #glm
-fit.glm <- train(as.factor(crime)~., data=train, method="glm",family=binomial(),
+fit.glm <- train(as.factor(label)~., data=train, method="glm",family=binomial(),
                  metric = "Accuracy", trControl = control)
 #random forest
-fit.rf <- train(as.factor(crime)~., data=train, method="rf", 
+fit.rf <- train(as.factor(label)~., data=train, method="rf", 
                 metric = "Accuracy", trControl = control)
 #boosting algorithm - Stochastic Gradient Boosting (Generalized Boosted Modeling)
-fit.gbm <- train(as.factor(crime)~., data=train, method="gbm", 
+fit.gbm <- train(as.factor(label)~., data=train, method="gbm", 
                  metric = "Accuracy", trControl = control)
 #svm
-fit.svm <- train(as.factor(crime)~., data=train, method="svmRadial", 
+fit.svm <- train(as.factor(label)~., data=train, method="svmRadial", 
                  metric = "Accuracy", trControl = control)
 #nnet
-fit.nnet <- train(as.factor(crime)~., data=train, method="nnet", 
+fit.nnet <- train(as.factor(label)~., data=train, method="nnet", 
                   metric = "Accuracy", trControl = control)
 #naive
-fit.naive <- train(as.factor(crime)~., data=train, 
+fit.naive <- train(as.factor(label)~., data=train, 
                    method="naive_bayes", metric = "Accuracy", 
                    trControl = control)
 #extreme gradient boosting
-fit.xgb <- train(as.factor(crime)~., data=train, 
+fit.xgb <- train(as.factor(label)~., data=train, 
                  method="xgbTree", metric = "Accuracy", trControl = control)
 #bagged cart
-fit.bg <- train(as.factor(crime)~., data=train, 
+fit.bg <- train(as.factor(label)~., data=train, 
                 method="treebag", metric = "Accuracy", trControl = control)
 #decision tree
-fit.dtree <- train(as.factor(crime)~., data=train, 
+fit.dtree <- train(as.factor(label)~., data=train, 
                    method="C5.0", metric = "Accuracy", trControl = control)
 #knn
-fit.knn <- train(as.factor(crime)~., data=train, 
+fit.knn <- train(as.factor(label)~., data=train, 
                  method="kknn", metric = "Accuracy", trControl = control)
 #ensemble
-fit.ensemble <- train(as.factor(crime)~., data=train, 
+fit.ensemble <- train(as.factor(label)~., data=train, 
                       method="nodeHarvest", metric = "Accuracy", trControl = control)
 
 stopCluster(cl)
@@ -134,11 +138,11 @@ bwplot(results)
 dotplot(results)
 
 # Model accuracy
-#mean(predicted.classes == test$crime)
+#mean(predicted.classes == test$label)
 #test data accuracy
 # Make predictions
 predicted.classes <- fit.knn %>% predict(test)
-output <- confusionMatrix(data = predicted.classes, reference = test$crime, mode = "everything")
+output <- confusionMatrix(data = predicted.classes, reference = test$label, mode = "everything")
 output
 #other metrics
 
@@ -148,9 +152,7 @@ caret::varImp(fit.rf)
 #write.csv(as.data.frame(output$byClass),"output.csv")
 
 #plot confusion matrix of selected option
-library(ggplot2)     # to plot
-library(gridExtra)   # to put more
-library(grid)        # plot together
+
 
 # plotting the matrix
 output2 <- as.data.frame(output$table)
