8dd775df05762053531d6373eaf1be3cfa7323f8
Edima <edimaudo@gmail.com>
2022-04-17 18:10:02 -0400

    Update amazon_review.R

--- R_analytics/amazon_reviews/amazon_review.R
+++ R_analytics/amazon_reviews/amazon_review.R
@@ -168,3 +168,31 @@ dotplot(results)
 predicted.classes <- fit.xgb %>% predict(test)
 output <- confusionMatrix(data = predicted.classes, reference = test$y, mode = "everything")
 
+
+
+
+
+review_words <- df %>%
+  unnest_tokens(word, Body) %>%
+  anti_join(stop_words) %>%
+  distinct() %>%
+  filter(nchar(word) > 3) 
+
+bing_word_counts <- review_words %>%
+  filter(input$productInput == 'B000GG87V2', 
+         input$countryInput == "United States ",
+         input$monthInput == "January",
+         input$ratingInput == Country,
+         input$yearInput == Year) %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+bing_word_counts %>%
+  group_by(sentiment) %>%
+  top_n(10) %>%
+  ggplot(aes(reorder(word, n), n, fill = sentiment)) +
+  geom_bar(alpha = 0.8, stat = "identity", show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(y = "Contribution to sentiment", x = NULL) +
+  coord_flip()
