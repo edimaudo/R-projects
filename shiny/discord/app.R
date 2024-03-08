c3bd9f9da7fdcff21b8197c934859019e9a57b4a
Edima <edimaudo@gmail.com>
2024-03-07 19:06:09 -0500

    Update app.R
    Added libraries

--- shiny/discord/app.R
+++ shiny/discord/app.R
@@ -8,9 +8,10 @@ rm(list = ls())
 ################
 packages <- c(
   'ggplot2', 'corrplot','tidyverse','shiny','shinydashboard','DT',
-  'mlbench','caTools','gridExtra','doParallel','grid',
+  'mlbench','caTools','gridExtra','doParallel','grid','forecast',
   'caret','dummies','mlbench','tidyr','Matrix','lubridate',
-  'data.table', 'rsample','scales'
+  'data.table', 'rsample','scales','stopwords','tidytext','stringr',
+  'reshape2', 'textmineR','topicmodels','textclean'
 )
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -23,3 +24,42 @@ for (package in packages) {
 #Load data
 ################
 df<- read.csv("Ocean Discord Data Challenge Dataset.csv")
+
+
+################
+#UI
+################
+ui <- dashboardPage(
+  dashboardHeader(title = "Ocean Discord Data Challenge"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("About", tabName = "about", icon = icon("th")),
+      menuItem("Overview", tabName = "overview", icon = icon("th")),
+      menuItem("Wine Quality Insights", tabName = "prediction", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "about",includeMarkdown("readme.md"),hr()),
+      tabItem(tabName = "overview",
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("wineTypeInput", "Wine Type", choices = c("Red","White"), selected = "Red"),
+                  submitButton("Submit")
+                ),
+                mainPanel(
+                  h3("Wine Quality distribution",style="text-align: center;"),
+                  plotOutput("WineQualityPlot"),
+                  h3("Wine Properties Correlation",style="text-align: center;"),
+                  plotOutput("WineCorrelationPlot"),  
+                  h3("Quality vs pH",style="text-align: center;"),
+                  plotOutput("QualitypHPlot"),
+                  h3("Quality vs Alcohol Content",style="text-align: center;"),
+                  plotOutput("QualityAlcoholPlot")
+                )
+              )
+      )
+    )
+   )
+  ) 
+ 
