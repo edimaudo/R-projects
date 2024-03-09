9da63eb27735823b8227efe7011019d833464d1a
Edima <edimaudo@gmail.com>
2024-03-08 20:27:05 -0500

    Update app.R
    Added Channel and Date setup

--- shiny/discord/app.R
+++ shiny/discord/app.R
@@ -11,7 +11,7 @@ packages <- c(
   'mlbench','caTools','gridExtra','doParallel','grid','forecast',
   'caret','dummies','mlbench','tidyr','Matrix','lubridate',
   'data.table', 'rsample','scales','stopwords','tidytext','stringr',
-  'reshape2', 'textmineR','topicmodels','textclean'
+  'reshape2', 'textmineR','topicmodels','textclean','plotly'
 )
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
@@ -25,11 +25,42 @@ for (package in packages) {
 ################
 df<- read.csv("Ocean Discord Data Challenge Dataset.csv")
 
-# channel setup
-channel <- sort(unique(df$Channel))
-channel <- c("All",channel)
 
-# Date setup
+################
+#Data Setup
+################
+
+# channel
+df <- df %>%
+  mutate(channel_category = case_when(df$Channel %in% c("Ocean Protocol - CORE-TECH - 🌊┃builders [1088751449271447552]",
+                                                        "Ocean Protocol - CORE-TECH - 🚀┃tech-announcements [1088412016621932588]",
+                                                        "Ocean Protocol - CORE-TECH - 🛳︱github [773926934601269248]") 
+                                      ~ 'CORE-TECH',
+                                      df$Channel  %in% c("Ocean Protocol - ECOSYSTEM - 🎨︱ai-fun [1088460971191832616]",
+                                                         "Ocean Protocol - ECOSYSTEM - 💻┃dev-lounge [971047506857300020]",
+                                                         "Ocean Protocol - ECOSYSTEM - 📈︱traders-lounge [1088458691671502890]",
+                                                         "Ocean Protocol - ECOSYSTEM - 📊︱data-science-hub [993828971408003152]",
+                                                         "Ocean Protocol - ECOSYSTEM - 🚜︱data-farming [993954438790258879]",
+                                                         "Ocean Protocol - ECOSYSTEM - 🤖︱predictoor [1151066755796574389]",
+                                                         "Ocean Protocol - ECOSYSTEM - 🪢︱ambassadors [769209726415929404]",
+                                                         "Ocean Protocol - ECOSYSTEM - 🪢︱treasure-hunter [1113013155514290228]") 
+                                      ~ 'ECOSYSTEM',
+                                      df$Channel  %in% c("Ocean Protocol - GENERAL - 🎉︱announcements [720631710785732638]",
+                                                         "Ocean Protocol - GENERAL - 🐦︱tweets [1162327153128525914]",
+                                                         "Ocean Protocol - GENERAL - 💡︱idea-factory [1088534285478269038]",
+                                                         "Ocean Protocol - GENERAL - 💭︱general-chat [612953349003673629]",
+                                                         "Ocean Protocol - GENERAL - 🗣┃ocean-lounge [1088136164713693258]",
+                                                         "Ocean Protocol - GENERAL - 🚨︱report-scammers [912281243012460594]")  
+                                      ~ 'GENERAL',
+                                      df$Channel  %in% c("Ocean Protocol - GET STARTED - 🌞︱gm [911643560594505809]",
+                                                         "Ocean Protocol - GET STARTED - 👋︱welcome [727898674637832274]",
+                                                         "Ocean Protocol - GET STARTED - 🖖︱introduce-yourself [747887740263333909]",
+                                                         "Ocean Protocol - GET STARTED - 🧐︱ask-the-ai [1082698926865522808]")
+                                      ~ 'GET STARTED'))
+
+channel_category_list <- sort(unique(df$channel))
+
+#Date setup
 df$Date2 <- lubridate::mdy(substring(df$Date,1,10))
 df$year <- lubridate::year(df$Date2)
 df$quarter <- lubridate::quarter(df$Date2)
